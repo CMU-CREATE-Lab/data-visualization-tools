ee7642a0b0dd324765c57fefc0a85ac583b61a01
Egil Moeller <egil.moller@freecode.no>
2014-01-07 17:01:57 +0100

    Export script for viirs

--- scripts/json-to-bin.py
+++ scripts/json-to-bin.py
@@ -7,15 +7,21 @@ import dateutil.parser
 with open(sys.argv[1]) as f:
     data = json.load(f)
 
+def conv(data, t, default):
+    try:
+        return t(data)
+    except:
+        return default
+
 with open(sys.argv[2], "w") as f:
     for d in data:
         f.write(struct.pack(
                 "<fffffii",
-                float(d["latitude"]),
-                float(d["longitude"]),
-                float(d["RadiantOutput"]),
-                float(d["RadiativeHeat"]),
-                float(d["footprint"]),
-                int(d["Temperature"]),
-                int(dateutil.parser.parse(d["datetime"]).strftime("%s"))
+                conv(d["latitude"], float, 0.0),
+                conv(d["longitude"], float, 0.0),
+                conv(d["RadiantOutput"], float, 0.0),
+                conv(d["RadiativeHeat"], float, 0.0),
+                conv(d["footprint"], float, 0.0),
+                conv(d["Temperature"], int, 0),
+                int(conv(d["datetime"], dateutil.parser.parse, 0).strftime("%s"))
                 ))
