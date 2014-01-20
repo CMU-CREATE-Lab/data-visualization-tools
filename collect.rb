798b8d270ec702341b1bbdb6b507edeb24e3442c
Egil Moeller <egil.moller@freecode.no>
2014-01-20 13:01:29 +0100

    Imported everything from drillviz repo

---
+++ collect.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby2.0
+
+load File.dirname(__FILE__) + "/../libs/utils.rb"
+
+def reload
+  load __FILE__
+end
+
+def main
+  wells = Dir.glob("../??/data/translated-??.json").flat_map do |file|
+    read_json file
+  end
+  wells.sort! {|a,b| a['Date'] <=> b['Date']}
+  #wells = wells.last(100000)
+  write_compact_json "wells.js.tmp", wells
+  open("wells.js", "w") do |out|
+    out.write "wells="
+    out.write open("wells.js.tmp"){|i| i.read}
+    out.puts ";"
+  end
+  File.unlink "wells.js.tmp"
+end
+
+main
