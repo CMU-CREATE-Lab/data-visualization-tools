953f1eb1d646ecfb06bb35c6d2aeebf120947f4b
Egil Moeller <egil.moller@freecode.no>
2014-01-23 16:24:50 +0100

    Added header to data format to make it more generic

--- js/utils.js
+++ js/utils.js
@@ -9,3 +9,81 @@ function LatLongToPixelXY(latitude, longitude) {
 
   return pixel;
 }
+
+function loadTypedMatrix(arrayBuffer) {
+  /* Loads a matrix of rows/cols of typed data from a binary file.
+     The data format is (all values little endian):
+
+     [4 byte header length in bytes]
+     [header data]
+     [row]
+     [row]
+     ...
+
+     The header is json encoded and should contain at the very least
+
+     {length: NUMBER_OF_ROWS, cols: [COL,...]}
+
+     COL should contain {name: NAME, type: TYPE}
+
+     where NAME is any string and TYPE is one of the type names found
+     in typemap below.
+
+     Each row consists of data encoded as per the column
+     specifications (in that same order). The byte length of each
+     column is defined by its type.
+  */
+
+  function arrayBuffer2String(buf) {
+    return String.fromCharCode.apply(null, new Uint8Array(buf));
+  }
+
+  typemap = {
+    'Int32': {'size': Int32Array.BYTES_PER_ELEMENT, 'array': Float32Array, 'method': 'getInt32'},
+    'Float32': {'size': Float32Array.BYTES_PER_ELEMENT, 'array': Float32Array, 'method': 'getFloat32'},
+  }
+
+  var dataView = new DataView(arrayBuffer);
+
+  var headerLen = dataView.getInt32(0, true);
+  var result = JSON.parse(arrayBuffer2String(arrayBuffer.slice(4, 4 + headerLen)));
+
+  dataView = new DataView(arrayBuffer, 4 + headerLen);
+
+  var rowLen = result.cols.map(function (col) {
+    return typemap[col.type].size;
+  }).reduce(
+    function (a, b) {
+      return a + b;
+    },
+    0
+  );
+
+  var rowCount = dataView.byteLength / rowLen ;
+  if (rowCount != result.length) {
+    throw "Data size does not match length specified in header: datasize/rowsize=" + rowCount.toString() + " header.length=" + result.length.toString();
+  }
+
+  for (var colidx = 0; colidx < result.cols.length; colidx++) {
+    var col = result.cols[colidx];
+    col.typespec = typemap[col.type];
+    col.data = new col.typespec.array(result.length);
+  };
+
+  for (var offset = 0, rowidx = 0; offset < dataView.byteLength; rowidx++) {
+    for (var colidx = 0; colidx < result.cols.length; colidx++) {
+      var col = result.cols[colidx];
+      col.data[rowidx] = dataView[col.typespec.method](offset, true);
+      offset += col.typespec.size;
+    }
+  }
+
+  var cols = {};
+  for (var colidx = 0; colidx < result.cols.length; colidx++) {
+    var col = result.cols[colidx];
+    delete col.typespec;
+    cols[col.name] = col;
+  }
+  result.cols = cols;
+  return result;
+}
--- scripts/json-to-bin.py
+++ scripts/json-to-bin.py
@@ -5,8 +5,12 @@ import struct
 import datetime
 import dateutil.parser
 
+print "Reading data..."
 with open(sys.argv[1]) as f:
     data = json.load(f)
+
+def timestamp(data):
+    return int(dateutil.parser.parse(data).strftime("%s"))
 
 def conv(data, t, default):
     try:
@@ -14,15 +18,64 @@ def conv(data, t, default):
     except:
         return default
 
+typemap = {
+    timestamp: 'Int32',
+    int: 'Int32',
+    float: 'Float32',
+    }
+
+typeformatmap = {
+    'Int32': 'i',
+    'Float32': 'f'
+    }
+typedefaultmap = {
+    'Int32': 0,
+    'Float32': 0.0
+    }
+
+print "Calculating header values..."
+
+datalen = len(data)
+cols = {}
+coltypes = {}
+for i, d in enumerate(data):
+    for key, value in d.iteritems():
+        if value == '__None__': continue
+        t = type(value)
+        if t is str or t is unicode:
+            # If it's clearly not a date, avoid the time to throw and exception...
+            if not value or value[0] not in '0123456789': continue
+            try:
+                value = timestamp(value)
+                t = timestamp
+            except:
+                continue
+        if key not in cols:
+            cols[key] = {'name': key, 'type': typemap[t], 'min': value, 'max': value}
+            coltypes[key] = t
+        cols[key]['max'] = max(cols[key]['max'], value)
+        cols[key]['min'] = min(cols[key]['min'], value)
+    if i % 1000 == 0:
+        print "%.2f%%" % (100 * float(i) / datalen)
+
+cols = cols.values()
+cols.sort(lambda a, b: cmp(a['name'], b['name']))
+header = {'cols': cols, 'length': len(data)}
+
+print "Header: ", header
+
+print "Writing data..."
+
 with open(sys.argv[2], "w") as f:
+    headerstr = json.dumps(header)
+    f.write(struct.pack("<i", len(headerstr)))
+    f.write(headerstr)
+
+    formatmap = '<' + ''.join(typeformatmap[col['type']] for col in cols)
+    colspecs = [{'name': col['name'], 'type': coltypes[col['name']], 'default': typedefaultmap[col['type']]} for col in cols]
+
     for d in data:
         f.write(struct.pack(
-                "<fffffii",
-                conv(d["latitude"], float, 0.0),
-                conv(d["longitude"], float, 0.0),
-                conv(d["RadiantOutput"], float, 0.0),
-                conv(d["RadiativeHeat"], float, 0.0),
-                conv(d["footprint"], float, 0.0),
-                conv(d["Temperature"], int, 0),
-                int(conv(d["datetime"], dateutil.parser.parse, 0).strftime("%s"))
-                ))
+                formatmap,
+                *[conv(d[colspec['name']], colspec['type'], colspec['default'])
+                  for colspec in colspecs]))
