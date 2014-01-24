e11736de3ac6f356cdcfdd8445908506ade61c6f
Gabriel ODonnell <gabriel.odonnell@gmail.com>
2014-01-24 12:42:20 -0500

    Update json-to-bin.rb script to current version

--- scripts/json-to-bin.rb
+++ scripts/json-to-bin.rb
@@ -1,4 +1,4 @@
-#!/usr/bin/env ruby1.9.1
+#!/usr/bin/env ruby2.0
 
 load File.dirname(__FILE__) + "/utils.rb"
 
@@ -10,7 +10,10 @@ class CustomFormatOptparse
   def self.parse(args)
     # defaults
     options = OpenStruct.new
-    options.cat = true
+    options.latKey = "latitude"
+    options.lngKey = "longitude"
+    options.dateKey = "date"
+    options.dateFormat = "unix"
 
     opt_parser = OptionParser.new do |opts|
       opts.banner = "Usage: custom-format.rb [options]"
@@ -22,12 +25,22 @@ class CustomFormatOptparse
         options.file = file
       end
 
-        # Boolean switch.
-      opts.on("-c", "--[no-]cat", "cat into single file") do |c|
-        options.cat = c
+      opts.on("--latKey [LATKEY]", "Key for latitude") do |latKey|
+        options.latKey = latKey
       end
 
-      
+      opts.on("--lngKey [LNGKEY]", "Key for longitude") do |lngKey|
+        options.lngKey = lngKey
+      end
+
+      opts.on("--dateKey [DATEKEY]", "Key for date") do |dateKey|
+        options.dateKey = dateKey
+      end
+
+      opts.on("--dateFormat [DATESTR]", "Date format string") do |dateFormat|
+        options.dateFormat = dateFormat
+      end
+
     end
 
     opt_parser.parse!(args)
@@ -41,59 +54,22 @@ def main
     puts "Could not find or open #{options.file}"
     return
   end
-  if options.cat
-    filename = File.basename(options.file, ".json") + ".bin"
-    bin = File.open(filename, 'wb')
-  else
-    latlonFilename =  File.basename(options.file, ".json") + "_latlon.bin"
-    latlonBin = File.open(latlonFilename, 'wb')
-
-    radiantOutputFilename = File.basename(options.file, ".json") + "_radiant_output.bin"
-    radiantOutputBin = File.open(radiantOutputFilename, 'wb')
-
-    radiativeHeatFilename = File.basename(options.file, ".json") + "_radiative_heat.bin"
-    radiativeHeatBin = File.open(radiativeHeatFilename, 'wb')
-
-    footprintFilename = File.basename(options.file, ".json") + "_footprint.bin"
-    footprintBin = File.open(footprintFilename, 'wb')
-
-    temperatureFilename = File.basename(options.file, ".json") + "_temperature.bin"
-    temperatureBin = File.open(temperatureFilename, 'wb')
-
-    datetimeFilename  = File.basename(options.file, ".json") + "_datetime.bin"
-    datetimeBin = File.open(datetimeFilename, 'wb')
-
-  end
+  latlonFilename =  File.basename(options.file, ".json") + "_latlon.bin"
+  latlonBin = File.open(latlonFilename, 'wb')
+  dateFilename =  File.basename(options.file, ".json") + "_date.bin"
+  dateBin = File.open(dateFilename, 'wb')  
   data = read_json(options.file)
   data.each do |d|
-    if options.cat
-      bin.write([d["latitude"].to_f].pack("e"))
-      bin.write([d["longitude"].to_f].pack("e"))
-      bin.write([d["RadiantOutput"].to_f].pack("e"))
-      bin.write([d["RadiativeHeat"].to_f].pack("e"))
-      bin.write([d["footprint"].to_f].pack("e"))
-      bin.write([d["Temperature"].to_i].pack("l<"))
-      bin.write([DateTime.strptime(d["datetime"], '%Y-%m-%d %H:%M:%S%z').to_time.to_i].pack("l<"))
+    latlonBin.write([d[options.latKey].to_f].pack("e"))
+    latlonBin.write([d[options.lngKey].to_f].pack("e"))
+    if options.dateFormat == "unix"
+      dateBin.write([d[options.dateKey].to_i].pack("l<"))
     else
-      latlonBin.write([d["latitude"].to_f].pack("e"))
-      latlonBin.write([d["longitude"].to_f].pack("e"))
-      radiantOutputBin.write([d["RadiantOutput"].to_f].pack("e"))
-      radiativeHeatBin.write([d["RadiativeHeat"].to_f].pack("e"))
-      footprintBin.write([d["footprint"].to_f].pack("e"))
-      temperatureBin.write([d["Temperature"].to_i].pack("l<"))
-      datetimeBin.write([DateTime.strptime(d["datetime"], '%Y-%m-%d %H:%M:%S%z').to_time.to_i].pack("l<"))
+      dateBin.write([DateTime.strptime(d[options.dateKey], options.dateFormat).to_time.to_i].pack("l<"))
     end
   end
-  if options.cat
-    bin.close
-  else
-    latlonBin.close
-    radiantOutputBin.close
-    radiativeHeatBin.close
-    footprintBin.close
-    temperatureBin.close
-    datetimeBin.close
-  end
+  latlonBin.close
+  dateBin.close
 end
 
 main
