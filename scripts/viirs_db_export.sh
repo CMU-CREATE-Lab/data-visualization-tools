636fe46e9388e2af19b5ee30a9800a23b903f97a
Egil Moeller <egil.moller@freecode.no>
2014-02-05 15:46:06 +0100

    Generate clusters

--- scripts/viirs_db_export.sh
+++ scripts/viirs_db_export.sh
@@ -5,12 +5,17 @@ DATADIR="$BINDIR/../data"
 
 source "$BINDIR/config.sh"
 
-mkdir -p "$DATADIR/"{csv,json,bin}
+mkdir -p "$DATADIR/"{csv,json,geojson,kml,bin}
 
 "$BINDIR/viirs_db_to_csv.sh" "$RADIUS" "$TIMEWINDOW" "$DETECTIONS" > "$DATADIR/csv/$NAME.csv"
+cluster --eps="$RADIUS" --min_samples="$DETECTIONS" "$DATADIR/csv/$NAME.csv" "$DATADIR/csv/$NAME.clusters.csv" 
 
 skyconvert "$DATADIR/csv/$NAME.csv" "$DATADIR/json/$NAME.json"
+skyconvert "$DATADIR/csv/$NAME.clusters.csv" "$DATADIR/geojson/$NAME.clusters.geojson"
+ogr2ogr -f KML "$DATADIR/kml/$NAME.clusters.kml" "$DATADIR/geojson/$NAME.clusters.geojson"
+
 gzip -f "$DATADIR/csv/$NAME.csv"
 "$BINDIR/json-to-bin.py" "$DATADIR/json/$NAME.json" "$DATADIR/bin/$NAME.bin"
 gsutil -m cp -z bin -a public-read -r "$DATADIR/bin/$NAME.bin" gs://$BUCKET/viirs/data/bin
 gsutil -m cp -a public-read -r "$DATADIR/csv/$NAME.csv.gz" gs://$BUCKET/viirs/data/csv
+gsutil -m cp -a public-read -r "$DATADIR/kml/$NAME.clusters.kml" gs://$BUCKET/viirs/data/kml
