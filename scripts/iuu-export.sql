6354e6a3d988c90d56fd2185862384899f43b20d
Egil Moeller <egil.moller@freecode.no>
2014-02-21 14:27:06 +0100

    Filter data bit more, and use score as magnitude

--- scripts/iuu-export.sql
+++ scripts/iuu-export.sql
@@ -7,7 +7,7 @@ create view ais_fishing_days as
     round(score * 256) as red,
     round((1 - score) * 256) as green,
     0 as blue,
-    magnitude * 256 as magnitude
+    score * 256 as magnitude
   from
     (select
        day.day,
@@ -20,6 +20,12 @@ create view ais_fishing_days as
        generate_series((select min(datetime) from appomatic_mapdata_ais), (select max(datetime) from appomatic_mapdata_ais), interval '1 day') as day
        join appomatic_mapdata_ais as ais on
          ais.sog <= 30
+         and ais.latitude > -36.39
+         and ais.latitude < -16.56
+         and ais.longitude > -121.48
+         and ais.longitude < -94.24
+         and abs(ais.latitude - -27.125996) > 0.1
+         and abs(ais.longitude - -109.358501) > 0.1
          and ais.datetime > day.day
          and ais.datetime < day + '1 days'
      group by day.day, ais.mmsi) as a;
