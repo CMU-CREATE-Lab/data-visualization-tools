f49c1a2733b63705157de7c55a584ef3f3ed596c
Egil Moeller <egil.moller@freecode.no>
2014-02-24 17:07:20 +0100

    Better data extraction

--- scripts/iuu-export.sql
+++ scripts/iuu-export.sql
@@ -14,10 +14,9 @@ create view ais_fishing_days as
        ais.mmsi,
        avg(ais.latitude) as latitude,
        avg(ais.longitude) as longitude,
-       sqrt(count(*)) / 45 as magnitude,
-       least(1, 2 * (avg(1 - sog/30) + stddev(sog / 30) + stddev(cog / 360)) / 3) as score
+       least(1, (coalesce(stddev(sog), 0) / 30 + coalesce(stddev(cog), 0) / 360) * 2) as score
      from
-       generate_series((select min(datetime) from appomatic_mapdata_ais), (select max(datetime) from appomatic_mapdata_ais), interval '1 day') as day
+       generate_series((select min(datetime) from appomatic_mapdata_ais), (select max(datetime) from appomatic_mapdata_ais), interval '3 hours') as day
        join appomatic_mapdata_ais as ais on
          ais.sog <= 30
          and ais.latitude > -36.39
@@ -26,6 +25,6 @@ create view ais_fishing_days as
          and ais.longitude < -94.24
          and abs(ais.latitude - -27.125996) > 0.1
          and abs(ais.longitude - -109.358501) > 0.1
-         and ais.datetime > day.day
-         and ais.datetime < day + '1 days'
+         and ais.datetime > day.day - '6 hours'::interval
+         and ais.datetime < day.day
      group by day.day, ais.mmsi) as a;
