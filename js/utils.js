d91ecf461581d7c858e7bc930bb57bdbe0e074f0
Egil Moeller <egil.moller@freecode.no>
2014-03-13 16:12:30 +0100

    Don't crash when attributes are optimized away, just warn

--- js/utils.js
+++ js/utils.js
@@ -337,10 +337,14 @@ function setParameter(name, value) {
 /* Load array data into gl buffers and bind that buffer to a shader
  * program attribute */
 function programLoadArray(gl, glbuffer, arraydata, program, attrname, size, type, stride, offset) {
-  gl.bindBuffer(gl.ARRAY_BUFFER, glbuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, arraydata, gl.STATIC_DRAW);
-  gl.enableVertexAttribArray(program.attributes[attrname]);
-  gl.vertexAttribPointer(program.attributes[attrname], size, type, false, stride || 0, offset || 0);
+  if (program.attributes[attrname] == undefined) {
+    console.warn(["Attempted to set an non-existent attribute " + attrname + ".", program]);
+  } else {
+    gl.bindBuffer(gl.ARRAY_BUFFER, glbuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, arraydata, gl.STATIC_DRAW);
+    gl.enableVertexAttribArray(program.attributes[attrname]);
+    gl.vertexAttribPointer(program.attributes[attrname], size, type, false, stride || 0, offset || 0);
+  }
 }
 
 function createShaderProgram(gl, vertexShaderNode, fragmentShaderNode) {
