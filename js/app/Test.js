0b4343b8bd63300026084936821d6bc8fb1e5a50
Egil Moeller <egil.moller@freecode.no>
2014-04-17 13:43:57 +0200

    Added some test for Events

--- js/app/Test.js
+++ js/app/Test.js
@@ -2,12 +2,54 @@ define(["Class", "QUnit", "LangExtensions"], function(Class, QUnit) {
   return Class({
     name: "Test",
     initialize: function () {
+      QUnit.config.testTimeout = 10000;
+
       QUnit.init();
       QUnit.start();
 
-      test( "hello test", function() {
-        ok( 1 == "1", "Passed!" );
+
+      QUnit.module("Events");
+
+      QUnit.asyncTest("Scope and argument passing", function() {
+        require(["Events"], function (Events) {
+          QUnit.expect(3);
+
+          var e = new Events("Test.Events");
+          var myscope = {};
+          var myarg = {};
+
+          e.on({
+            someEvent: function (arg) {
+              QUnit.equal(this, myscope, "Scope isn't set to the provided scope");
+              QUnit.equal(arg, myarg, "Argument isn't set to the provided argument");
+              QUnit.ok(true, "Events triggered as they should");
+              start();
+            },
+            scope: myscope
+          });
+          e.triggerEvent("someEvent", myarg);
+        });
       });
+
+      QUnit.asyncTest("Recursive events", function() {
+        require(["Events"], function (Events) {
+          QUnit.expect(1);
+
+          var e = new Events("Test.Events");
+
+          e.on({
+            firstEvent: function (arg) {
+              e.triggerEvent("secondEvent");
+            },
+            secondEvent: function (arg) {
+              QUnit.ok(true, "Events triggered as they should");
+              start();
+            }
+          });
+          e.triggerEvent("firstEvent");
+        });
+      });
+
     }
   });
 });
