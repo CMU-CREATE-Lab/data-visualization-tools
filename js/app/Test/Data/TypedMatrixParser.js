ab70542389dd065c065d050142520f8318c0388c
Egil Moeller <egil.moller@freecode.no>
2014-04-24 15:36:38 +0200

    Unit test for parsing

--- js/app/Test/Data/TypedMatrixParser.js
+++ js/app/Test/Data/TypedMatrixParser.js
@@ -2,21 +2,29 @@ define(["Class", "QUnit", "Test/BaseTest", "Data/TypedMatrixParser", "Data/Typed
   return Class(BaseTest, {
     name: "TypedMatrixParser",
 
-    "Generate and parse": function (cb) {
-      QUnit.expect(7);
+    "Generate data": function (cb) {
+      QUnit.expect(1);
 
-      var cols = {foo: new Int32Array(3), bar: new Float32Array(3)};
+      var cols = {foo: new Int32Array(3), bar: new Int32Array(3)};
       cols.foo[0] = 4;
       cols.foo[1] = 5;
       cols.foo[2] = 6;
-      cols.bar[0] = 3.99;
-      cols.bar[1] = 4.99;
-      cols.bar[2] = 5.99;
+      cols.bar[0] = 8;
+      cols.bar[1] = 9;
+      cols.bar[2] = 10;
       var g = new TypedMatrixGenerator({test: 4711, colsByName: {foo: {gazonk: 3}}}, cols);
-      var data = g.asURI();
-      QUnit.ok(true, "Generated data");
 
-      var p = new TypedMatrixParser(data);
+      var data = g.asBin();
+      $.get(require.toUrl("Test/Data/foo.bin"), function (filedata) {
+        QUnit.equal(data, filedata, "Generated data matches stored good data");
+        cb();
+      });
+    },
+
+    "Parse generated data": function (cb) {
+      QUnit.expect(6);
+
+      var p = new TypedMatrixParser(require.toUrl("Test/Data/foo.bin"));
       var rowidx = 0;
       p.events.on({
         header: function (data) {
@@ -26,8 +34,8 @@ define(["Class", "QUnit", "Test/BaseTest", "Data/TypedMatrixParser", "Data/Typed
         },
         row: function (data) {
           if (rowidx == 2) {
-            QUnit.equal(data.foo, 6, "Integer column value");
-            QUnit.equal(data.bar, 5.99, "Float column value");
+            QUnit.equal(data.foo, 6, "First value matches");
+            QUnit.equal(data.bar, 10, "Other value matches");
           }
           rowidx++;
         },
