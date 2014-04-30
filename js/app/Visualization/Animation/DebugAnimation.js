cc10ef28f34b0269d5d69230288f8b260bf35412
Egil Moeller <egil.moller@freecode.no>
2014-04-30 13:37:25 +0200

    Added a simplistic debug animation that plots lat/lon and nothing else

---
+++ js/app/Visualization/Animation/DebugAnimation.js
@@ -0,0 +1,59 @@
+define(["require", "Class", "Visualization/GeoProjection", "Visualization/Shader", "Visualization/Animation/Animation"], function(require, Class, GeoProjection, Shader, Animation) {
+  var DebugAnimation = Class(Animation, {
+    name: "DebugAnimation",
+
+    initGl: function(gl, cb) {
+      var self = this;
+      self.gl = gl;
+      Shader.createShaderProgramFromUrl(
+        self.gl,
+        require.toUrl("Visualization/Animation/DebugAnimation-vertex.glsl"),
+        require.toUrl("Visualization/Animation/DebugAnimation-fragment.glsl"),
+        function (program) {
+          self.program = program;
+
+          self.pointArrayBuffer = self.gl.createBuffer();
+
+          cb();
+        }
+      );
+    },
+
+    updateData: function() {
+      var self = this;
+      var format = self.manager.visualization.data.format;
+      var header = format.header;
+      var data = format.data;
+      self.pointcount = header.length;
+
+      self.rawLatLonData = new Float32Array(self.pointcount*2);
+
+      for (var rowidx = 0; rowidx < self.pointcount; rowidx++) {
+        var pixel = GeoProjection.LatLongToPixelXY(data.latitude[rowidx], data.longitude[rowidx]);
+        self.rawLatLonData[2*rowidx] = pixel.x;
+        self.rawLatLonData[2*rowidx+1] = pixel.y;
+      }
+
+      self.gl.useProgram(self.program);
+      Shader.programLoadArray(self.gl, self.pointArrayBuffer, self.rawLatLonData, self.program);
+
+      Animation.prototype.updateData.call(self);
+    },
+
+    draw: function () {
+      var self = this;
+
+      self.gl.useProgram(self.program);
+
+      Shader.programBindArray(self.gl, self.pointArrayBuffer, self.program, "worldCoord", 2, self.gl.FLOAT);
+      self.gl.uniformMatrix4fv(self.program.uniforms.mapMatrix, false, self.manager.mapMatrix);
+
+      self.gl.drawArrays(self.gl.POINTS, 0, self.pointcount);
+
+      Animation.prototype.draw.call(self);
+    }
+  });
+  Animation.animationClasses.debug = DebugAnimation;
+
+  return DebugAnimation;
+});
