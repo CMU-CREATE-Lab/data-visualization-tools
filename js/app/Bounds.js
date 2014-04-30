48cfb20867e9aa3a9f5a26b3df21d5aa5dd3530a
Egil Moeller <egil.moller@freecode.no>
2014-04-30 11:31:20 +0200

    Added some debugging code for tiles

--- js/app/Bounds.js
+++ js/app/Bounds.js
@@ -1,3 +1,7 @@
+/* Bounds code adapted from OpenLayers; Published under the 2-clause
+ * BSD license. See license.txt in the OpenLayers distribution.
+ *
+ * This is a minimalistic subset of the Openlayers.Bounds API. */
 define(["Class"], function(Class) {
   return Class({
     name: "Bounds",
@@ -22,6 +26,20 @@ define(["Class"], function(Class) {
     toBBOX: function () {
       var self = this;
       return self.left + "," + self.bottom + "," + self.right + "," + self.top;
+    },
+
+    contains:function(x, y, inclusive) {
+      if (inclusive == undefined) {
+        inclusive = true;
+      }
+
+      if (inclusive) {
+        return ((x >= this.left) && (x <= this.right) && 
+                (y >= this.bottom) && (y <= this.top));
+      } else {
+        return ((x > this.left) && (x < this.right) && 
+                (y > this.bottom) && (y < this.top));
+      }
     }
   });
 });
--- js/app/Data/Tile.js
+++ js/app/Data/Tile.js
@@ -7,6 +7,30 @@ define(["Class", "Data/BinFormat"], function(Class, BinFormat) {
       self.bounds = bounds;
 
       BinFormat.prototype.initialize.call(self, self.manager.source + "/" + self.bounds.toBBOX());
+    },
+    verify: function () {
+      var self = this;
+      var res = {
+        real: {
+          outside: 0,
+          inside: 0
+        },
+        virtual: {
+          outside: 0,
+          inside: 0
+        },
+        total: self.rowcount
+      };
+
+      var namebyvirt = {false: 'real', true: 'virtual'};
+      var namebyinout = {false: 'outside', true: 'inside'};
+
+      for (var i = 0; i < self.rowcount; i++) {
+        var virtname = namebyvirt[self.data.virtual && !!self.data.virtual[i]];
+        var inoutname = namebyinout[self.bounds.contains(self.data.longitude[i], self.data.latitude[i])];
+        res[virtname][inoutname]++;
+      }
+      return res;
     }
   });
 });
