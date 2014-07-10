143d04980f3dab263f34d6454bdd3dfbd5d40bb9
Egil Moeller <egil.moller@freecode.no>
2014-07-10 16:56:25 +0200

    Webworker documentation

--- js/webworker.js
+++ js/webworker.js
@@ -1,3 +1,8 @@
+/* Usage:
+ *
+ * w = new Worker("/viirs/js/webworker.js")
+ * w.postMessage({useDojo: false, main:"(function () { require(['app/Class'], function (Class) { console.log('hello'); }); })"})
+ */
 (function () {
   var handler = function(e) {
     self.removeEventListener('message', handler, false);
