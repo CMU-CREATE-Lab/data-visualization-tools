8e6a0c7f694366f31a9a42e75e233c8434ba29b6
Randy Sargent <randy.sargent@gmail.com>
2014-11-08 12:03:22 -0500

    Use google tiles, for now

--- examples/webgl-timemachine/WebglTimeMachineLayer.js
+++ examples/webgl-timemachine/WebglTimeMachineLayer.js
@@ -1,14 +1,15 @@
 "use strict";
 
-function WebglTimeMachineLayer(glb, canvasLayer) {
+function WebglTimeMachineLayer(glb, canvasLayer, rootUrl) {
   this.glb = glb;
   this.gl = glb.gl;
   this._canvasLayer = canvasLayer;
+  this._rootUrl = rootUrl;
   
   var r = {"fps":10.0,"frames":29,"height":1335834,"leader":0,"level_info":[{"cols":1,"rows":1},{"cols":1,"rows":4},{"cols":5,"rows":11},{"cols":12,"rows":24},{"cols":27,"rows":50},{"cols":56,"rows":102},{"cols":115,"rows":206},{"cols":232,"rows":415},{"cols":467,"rows":832},{"cols":936,"rows":1667},{"cols":1874,"rows":3337},{"cols":3750,"rows":6677}],"level_scale":2.0,"nlevels":12,"tile_height":200,"tile_width":356,"video_height":800,"video_width":1424,"width":1335834};
   
   function createTile(ti, bounds) {
-    var url = '1068x600/' + ti.l + '/' + (ti.r * 4) + '/' + (ti.c * 4) + '.mp4';
+    var url = rootUrl + '/' + ti.l + '/' + (ti.r * 4) + '/' + (ti.c * 4) + '.mp4';
     return new WebglVideoTile(glb, ti, bounds, url);
   }
   
