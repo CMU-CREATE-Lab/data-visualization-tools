50c174713f281471c6905d390954cac69d38f02b
Gabriel ODonnell <gabriel.odonnell@gmail.com>
2015-11-20 15:14:21 -0500

    [examples/webgl-timemachine] Coral reef location tiles   * WIP   * CopyPasta from vector tiles for WDPA

---
+++ examples/webgl-timemachine/WebglVectorTile2.js
@@ -0,0 +1,122 @@
+"use strict";
+
+//
+// Want to quadruple-buffer
+// From time 1 to 1.999, display 1
+//                       already have 2 in the hopper, nominally
+//                       be capturing 3
+//                       have a fourth fallow buffer to let pipelined chrome keep drawing
+
+// Be capturing 3 means that at t=1, the first video just crossed 3.1,
+//                   and that at t=1.999, the last video just crossed 3.1
+// So we're aiming to run the videos at current display time plus 1.1 to 2.1
+// Or maybe compress the range and go with say 1.6 to 2.1?  That lets us better use
+// the flexibility of being able to capture the video across a range of times
+
+function WebGLVectorTile2(glb, tileidx, bounds, url) {
+  this.glb = glb;
+  this.gl = glb.gl;
+  this._tileidx = tileidx;
+  this._bounds = bounds;
+  this._url = url;
+  this._ready = false;
+  this.program = glb.programFromSources(WebGLVectorTile2.vectorTileVertexShader,
+    WebGLVectorTile2.vectorTileFragmentShader);
+  this._load();
+}
+
+WebGLVectorTile2.prototype
+._load = function() {
+  var that = this;
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', that._url);
+  xhr.responseType = 'arraybuffer';
+  xhr.onload = function() {
+    var float32Array = new Float32Array(this.response);
+    that._setData(float32Array);
+  }
+  // If tile 404's, replace with defaultUrl.  This lets us remove e.g. all the
+  // sea tiles and replace with a single default tile.
+  xhr.onerror = function() {
+    that._setData(new Float32Array([]));
+  }
+  xhr.send();
+}
+
+WebGLVectorTile2.prototype.
+_setData = function(arrayBuffer) {
+  var gl = this.gl;
+  this._pointCount = arrayBuffer.length / 2;
+
+  this._data = arrayBuffer;
+  this._arrayBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, this._arrayBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, this._data, gl.STATIC_DRAW);
+
+  var attributeLoc = gl.getAttribLocation(this.program, 'worldCoord');
+  gl.enableVertexAttribArray(attributeLoc);
+  gl.vertexAttribPointer(attributeLoc, 2, gl.FLOAT, false, 8, 0);
+
+  this._ready = true;
+}
+
+WebGLVectorTile2.prototype.
+isReady = function() {
+  return this._ready;
+}
+
+WebGLVectorTile2.prototype.
+delete = function() {
+  console.log('delete');
+}
+
+WebGLVectorTile2.prototype.
+draw = function(transform) {
+  var gl = this.gl;
+  if (this._ready) {
+    gl.lineWidth(2);
+    gl.useProgram(this.program);
+
+    var tileTransform = new Float32Array(transform);
+
+
+  scaleMatrix(tileTransform, Math.pow(2,this._tileidx.l)/256., Math.pow(2,this._tileidx.l)/256.);
+
+  translateMatrix(tileTransform, (this._bounds.max.x - this._bounds.min.x)/256., (this._bounds.max.y - this._bounds.min.y)/256.);
+  scaleMatrix(tileTransform, this._bounds.max.x - this._bounds.min.x, this._bounds.max.y - this._bounds.min.y);
+
+    this.transform = tileTransform;
+    var matrixLoc = gl.getUniformLocation(this.program, 'mapMatrix');
+    gl.uniformMatrix4fv(matrixLoc, false, tileTransform);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, this._arrayBuffer);
+
+    var attributeLoc = gl.getAttribLocation(this.program, 'worldCoord');
+    gl.enableVertexAttribArray(attributeLoc);
+    gl.vertexAttribPointer(attributeLoc, 2, gl.FLOAT, false, 8, 0);
+
+    gl.drawArrays(gl.LINES, 0, this._pointCount);
+  }
+}
+
+// Update and draw tiles
+WebGLVectorTile2.update = function(tiles, transform) {
+  for (var i = 0; i < tiles.length; i++) {
+    tiles[i].draw(transform);
+  }
+}
+
+
+WebGLVectorTile2.vectorTileVertexShader =
+'attribute vec4 worldCoord;\n' +
+
+'uniform mat4 mapMatrix;\n' +
+
+'void main() {\n' +
+'    gl_Position = mapMatrix * worldCoord;\n' +
+'}';
+
+WebGLVectorTile2.vectorTileFragmentShader =
+'void main() {\n' +
+'  gl_FragColor = vec4(.0, 1., .15, 1.0);\n' +
+'}\n';
