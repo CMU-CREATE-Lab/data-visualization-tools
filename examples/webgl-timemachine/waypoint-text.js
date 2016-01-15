a2a6173f6c3f2e7f0cf72c6434db2e2ca1244791
Paul Dille <pdille@cmucreatelab.org>
2016-01-15 00:44:39 -0500

    - Added descriptions to all the waypoints - Some fixes

--- examples/webgl-timemachine/waypoint-text.js
+++ examples/webgl-timemachine/waypoint-text.js
@@ -1,6 +1,487 @@
 WAYPOINT_TEXT = [
   {
     "title" : "",
-    "text" : "The video has one frame per year"
+    "text" : ""
+  },
+  {
+    "title" : "",
+    "text" : ""
+  },
+  {
+    "title" : "Agricultural fires",
+    "text" : "Seasonal fires in Africa include range burning to encourage tender grass growth for grazing, or in preparation for planting."
+  },
+  {
+    "title" : "Agricultural fires",
+    "text" : "Fires in South East Asia move south to north annually in preparation for the growing season."
+  },
+  {
+    "title" : "Drilling Flares",
+    "text" : "Methane and other light products flare at oil wells across Northern Africa and the Middle East."
+  },
+  {
+    "title" : "Shale oil extraction",
+    "text" : "Fracked oil wells in the US produce a large fraction of methane and other light gases. In North Dakota, approximately one third of the carbon extracted from the ground is converted directly into CO2 via flaring."
+  },
+  {
+    "title" : "Eagle Downs Coal Mine",
+    "text" : "30 years of excavation at the Eagle Downs open pit coal mine in Queensland, Australia. Most Australian coal is exported to China."
+  },
+  {
+    "title" : "Mountaintop Removal",
+    "text" : "Mountaintop Removal in West Virginia involves moving aside the tops of mountains to access coal seams. This practice often poisons water."
+  },
+  {
+    "title" : "Alberta Tar Sands",
+    "text" : "Tar Sands extraction in Alberta, Canada.  Half of the tar extracted is burned to process the other half."
+  },
+  {
+    "title" : "Shanghai",
+    "text" : "Shanghai merges into nearby cities, growing to become the 6th largest megacity, with 24 million people."
+  },
+  {
+    "title" : "Dubai",
+    "text" : "Dubai Palm and World Islands."
+  },
+  {
+    "title" : "Dallas Fort-Worth",
+    "text" : ""
+  },
+  {
+    "title" : "Dallas Fort-Worth",
+    "text" : "Dallas Fort-Worth growth over 25 years."
+  },
+  {
+    "title" : "United States",
+    "text" : "Red rings reveal growing Atlanta, Dallas-Forth Worth, Phoenix."
+  },
+  {
+    "title" : "Lights at Night trends",
+    "text" : "Red shows strong growth in Asia."
+  },
+  {
+    "title" : "East Asia",
+    "text" : "In contrast to Japan, China shows strong growth."
+  },
+  {
+    "title" : "Rondonia",
+    "text" : "Conversion of forests to farms in Rondonia, Brazil."
+  },
+  {
+    "title" : "Rondonia",
+    "text" : "Conversion of forests to farms in Rondonia, Brazil."
+  },
+  {
+    "title" : "Amazon Rain Forest",
+    "text" : "Arc of deforestation, Amazon Rain Forest."
+  },
+  {
+    "title" : "Amazon Rain Forest",
+    "text" : "Yellower tones indicate slowing rate of deforestation, while redder tones indicate speeding rate."
+  },
+  {
+    "title" : "Columbia Glacier",
+    "text" : "Columbia Glacier has retreated more than 20 kilometers."
+  },
+  {
+    "title" : "Mendenhall Glacier",
+    "text" : "Mendenhall Glacier has retreated about 1 kilometer."
+  },
+  {
+    "title" : "Lake Urmia (Iran)",
+    "text" : "Water previously terminating in Lake Urmia is diverted for agricultural."
+  },
+  {
+    "title" : "Aral Sea (Asia)",
+    "text" : "Water previously terminating in Aral Sea is diverted for agricultural. The region is also experiencing drought."
+  },
+  {
+    "title" : "Aral Expansion",
+    "text" : "Cropland in Uzbekistan south of Aral Sea expands and retreats."
+  },
+  {
+    "title" : "Australia Bushfires",
+    "text" : "Climate change is increasing the frequency and severity of bushfires in Australia."
+  },
+  {
+    "title" : "Bark Beetles",
+    "text" : "Bark beetle range is increases during warmer winters, leading to widespread tree death in Colorado."
+  },
+  {
+    "title" : "Rondonia",
+    "text" : "Indigenous areas and preserves can halt or slow deforestation in Brazil."
+  },
+  {
+    "title" : "Gansu Wind Farm",
+    "text" : "Gansu Wind Farm, China: 8GW (20GW planned)"
+  },
+  {
+    "title" : "Solar Star",
+    "text" : "Solar Star, US: 579MW photovoltaic"
+  },
+  {
+    "title" : "",
+    "text" : ""
+  },
+  {
+    "title" : "Alabama",
+    "text" : "A 2011 tornado path from Tuscaloosa to Birmingham, Alabama, USA is shown in red.  Tornado frequency in the southeast USA is expected to increase due to global warming."
+  },
+  {
+    "title" : "Yakutsk",
+    "text" : "Boreal forest fire dynamics are illustrated for Yakutsk, eastern Siberia, Russia.  Fire frequency, extent and severity are likely to increase with global warming."
+  },
+  {
+    "title" : "Mozambique",
+    "text" : "Most of African land change is due to smallholder agriculture, where families clear woodlands and forest to grow crops.  The sustainability of such systems given burgeoning populations is threatened as they require fallow periods for land to recover before subsequent clearing.  The threats to biodiversity are great as humans encroach on the remaining natural woodland and forest ecosystems."
+  },
+  {
+    "title" : "Indonesia",
+    "text" : "Industrial scale clearing of tropical forests to produce palm oil and wood products is clear in Riau, Sumatra, Indonesia.  Palm oil is found in many household products and foods and is produced only in the humid tropics.  Humid tropical forests are the most biodiverse on the planet and their replacement by monoculture palm estates and tree plantations results in ecosystem collapse."
+  },
+  {
+    "title" : "Paraguay",
+    "text" : "Paraguay is the site of one of the highest deforestation rates globally, from its Interior Atlantic Forests in the east to Chaco woodlands in the west.  Land is cleared for row crops or pastures, with little area set aside for nature or indigenous reserves. "
+  },
+  {
+    "title" : "Cote D'Ivoire",
+    "text" : "Forest loss within protected areas in Cote D'Ivoire is evident and related to the two civil wars of 2002 and 2007 and a resulting breakdown in governance.  The dynamic points out the fragility of protected area networks which on paper can cover a substantial area of a given country, but are subject to exploitation in times of conflict."
+  },
+  {
+    "title" : "Kalimantan",
+    "text" : " Like Sumatra, Kalimantan, Indonesia is the site of large-scale agroindustrial clearing.  Kalimantan's peatlands, habitat for orangutans and extensive carbon stores in peatland soils, are increasingly appropriated for palm oil production.  The interior highlands are logged, but not cleared as extensively as accessible lowland and wetland forests."
+  },
+  {
+    "title" : "Sarawak",
+    "text" : "The border of Malaysia and Indonesia on the island of Borneo reveals a variation in forest exploitation between the two countries.  Interior Borneo is the site of intensive high-grading of high value tropical forests on the Malaysian side of the border.  The Heart of Borneo conservation initiative will likely rely on the Indonesian side of the border for conserving the unique ecosystem services of montane Borneo forests."
+  },
+  {
+    "title" : "Russia/Finland",
+    "text" : "Under Soviet rule, a buffer of forest was maintained along the Russian-Finland border.  Recent access to this forest under Russian governance has led to increased forest clearing.  The Finland side of the border reflects a long-held smallholder forestry land use, and a lack of intact natural forests."
+  },
+  {
+    "title" : "",
+    "text" : ""
+  },
+  {
+    "title" : "Las Vegas",
+    "text" : "Las Vegas grows while Lake Mead shrinks."
+  },
+  {
+    "title" : "Lake Urmia (Iran)",
+    "text" : "Water previously terminating in Lake Urmia is diverted for agricultural."
+  },
+  {
+    "title" : "Aral Sea (Asia)",
+    "text" : "Water previously terminating in Aral Sea is diverted for agricultural.  Region is also experiencing drought."
+  },
+  {
+    "title" : "Aral Expansion",
+    "text" : "Cropland in Uzbekistan south of Aral Sea expands and retreats."
+  },
+  {
+    "title" : "Saudi Irrigation",
+    "text" : "Cropland in Saudi Arabia is irrigated from ancient aquifer."
+  },
+  {
+    "title" : "Central Valley",
+    "text" : "The productive California Central Valley relies on irrigation."
+  },
+  {
+    "title" : "California Reservoirs",
+    "text" : "Pedro Reservoir and Lake McClure lose water during recent drought."
+  },
+  {
+    "title" : "Bolivia Meander",
+    "text" : "Tributary of the Amazon fresh from the Andes meaders in Bolivia."
+  },
+  {
+    "title" : "Bolivia Avulsion",
+    "text" : "Tributary of the Amazon changes path, depositing soil across large area."
+  },
+  {
+    "title" : "Brazil Reservoir",
+    "text" : "Lago de Serra da Mesa, Brazil."
+  },
+  {
+    "title" : "Amazon Deforestation",
+    "text" : "Amazon forest loss diminishes precipitation."
+  },
+  {
+    "title" : "Bangladesh",
+    "text" : "Creation of fish ponds in Bangladesh."
+  },
+  {
+    "title" : "Fish Ponds",
+    "text" : "Fish ponds, Bohai Sea, China"
+  },
+  {
+    "title" : "Coral Reef Watch",
+    "text" : "Ocean heating, El Nino, and acidification from CO2 have triggered a massive coral bleaching event."
+  },
+  {
+    "title" : "",
+    "text" : ""
+  },
+  {
+    "title" : "Shanghai",
+    "text" : "Shanghai merges into nearby cities, growing to become the 6th largest megacity, with 24 million people."
+  },
+  {
+    "title" : "Dallas Fort-Worth",
+    "text" : ""
+  },
+  {
+    "title" : "Barnett Shale",
+    "text" : "Fracking the Barnett Shale for Natural Gas."
+  },
+  {
+    "title" : "DFW Growth",
+    "text" : "Dallas Fort-Worth growth over 25 years"
+  },
+  {
+    "title" : "United States",
+    "text" : "Red rings reveal growing Atlanta, Dallas-Forth Worth, Phoenix"
+  },
+  {
+    "title" : "Lights at Night trends",
+    "text" : "Red shows strong growth in Asia"
+  },
+  {
+    "title" : "Pearl River Delta, China",
+    "text" : ""
+  },
+  {
+    "title" : "Fires At Night",
+    "text" : ""
+  },
+  {
+    "title" : "Drilling Flares",
+    "text" : "Methane and other light products flare at oil wells across Northern Africa and the Middle East."
+  },
+  {
+    "title" : "Shale oil extraction",
+    "text" : "Fracked oil wells in the US produce a large fraction of methane and other light gases. In North Dakota, approximately one third of the carbon extracted from the ground is converted directly into CO2 via flaring."
+  },
+  {
+    "title" : "Wyoming Coal Mining",
+    "text" : "Open pit coal mining in Wyoming, US."
+  },
+  {
+    "title" : "Mountaintop Removal",
+    "text" : "Mountaintop Removal in West Virginia involves dumping tops of mountains into valleys to access coal seams, often polluting watersheds."
+  },
+  {
+    "title" : "Mountaintop Removal",
+    "text" : "Mountaintop Removal in West Virginia involves moving aside the tops of mountains to access coal seams. This practice often poisons water."
+  },
+  {
+    "title" : "Hei Dai Gao coal",
+    "text" : "Open pit coal mining in China."
+  },
+  {
+    "title" : "Alberta Tar Sands",
+    "text" : "Tar Sands extraction in Alberta, Canada.  Half of the tar extracted is burned to process the other half."
+  },
+  {
+    "title" : "Peru Mining",
+    "text" : "Tailings from artisinal gold mining in Peru."
+  },
+  {
+    "title" : "Grasberg Mine",
+    "text" : "Grasberg Mine in Indonesia is the largest gold mine and third largest copper mine."
+  },
+  {
+    "title" : "Grasberg Tailings",
+    "text" : "Tailings from Grasberg Mine."
+  },
+  {
+    "title" : "Aral Sea (Asia)",
+    "text" : "Water previously terminating in Aral Sea is diverted for agricultural.  Region is also experiencing drought."
+  },
+  {
+    "title" : "Saudi Irrigation",
+    "text" : "Cropland in Saudi Arabia is irrigated from ancient aquifer."
+  },
+  {
+    "title" : "Rondonia",
+    "text" : "Conversion of forests to farms in Rondonia, Brazil."
+  },
+  {
+    "title" : "Rondonia",
+    "text" : "Conversion of forests to farms in Rondonia, Brazil."
+  },
+  {
+    "title" : "Amazon Rain Forest",
+    "text" : "Arc of deforestation, Amazon Rain Forest."
+  },
+  {
+    "title" : "Amazon Rain Forest",
+    "text" : "Yellower tones indicate slowing rate of deforestation, while redder tones indicate speeding rate."
+  },
+  {
+    "title" : "Indonesia Forests",
+    "text" : "Forest loss in Indonesia is accelerating."
+  },
+  {
+    "title" : "Sumatra fires",
+    "text" : "Fires in Sumatra, Indonesia remove forest for agriculture."
+  },
+  {
+    "title" : "Forestry, South East US",
+    "text" : "Forestry in Southeast United States supplies paper pulp and biomass for power generation."
+  },
+  {
+    "title" : "Longyangxia Solar",
+    "text" : "Longyangxia Dam Solar Park: 320MW photovoltaic"
+  },
+  {
+    "title" : "Solar Star",
+    "text" : "Solar Star, US:  579MW photovoltaic"
+  },
+  {
+    "title" : "Topaz Solar Farm",
+    "text" : "Topaz Solar Farm, US: 550MW photovoltaic"
+  },
+  {
+    "title" : "Charanka Solar Park",
+    "text" : "Charanka Solar Park, India: 600MW photovoltaic"
+  },
+  {
+    "title" : "Ivanpah Solar Power",
+    "text" : "Ivanpah Solar Power, US: Largest concentrated solar power, 392MW"
+  },
+  {
+    "title" : "Valle Solar Power Station",
+    "text" : "Valle Solar, Spain: 100MW concentrated solar"
+  },
+  {
+    "title" : "Gansu Wind Farm",
+    "text" : "Gansu Wind Farm, China: 8GW (20GW planned)"
+  },
+  {
+    "title" : "Alta Wind Farm",
+    "text" : "Alta Wind Farm, US: 1.5GW"
+  },
+  {
+    "title" : "US Wind",
+    "text" : "Wind turbine installation across the United States."
+  },
+  {
+    "title" : "US Photovoltaic",
+    "text" : "Solar photovoltaic installations across the United States."
+  },
+  {
+    "title" : "",
+    "text" : ""
+  },
+  {
+    "title" : "Pittsburgh",
+    "text" : "Pittsburgh, USA, demonstrates long-term exurban growth, with significant greenspace loss rather than through densification."
+  },
+  {
+    "title" : "Pittsburgh",
+    "text" : "Pittsburgh's evolution demonstrates residential and industrial sites intermingled, bringing questions of airshed and watershed toxicity to the forefront as major emissions, residences and schools all collocate."
+  },
+  {
+    "title" : "Lagos",
+    "text" : "Lagos, Nigeria has witnessed massive growth, from 17 million in 2006 to over 20 million today."
+  },
+  {
+    "title" : "Lagos",
+    "text" : "Living resources of water and forest in Lagos, Nigeria are threatened by massive growth due to economic opportunity. This combination demands thoughtful and long-term planning."
+  },
+  {
+    "title" : "Seoul",
+    "text" : "Seoul, Korea demonstrated long-term urban protected area planning with regions such as Bukhansan National Park, preserving greenspace during urban densification."
+  },
+  {
+    "title" : "Seoul",
+    "text" : "Seoul, Korea demonstrated long-term urban protected area planning with regions such as Bukhansan National Park, preserving greenspace during urban densification."
+  },
+  {
+    "title" : "Shenzhen",
+    "text" : "The Pearl River Delta in China has witnessed massive population explosions, with Shenzhen growing from a mere 40,000 inhabitants to 40 million in three decades. Massive land use transformations can be seen in concert with this rapid growth."
+  },
+  {
+    "title" : "Brasilia",
+    "text" : "Brasilia, Brazil includes the largest national urban park in the world, protecting tropical savanna and forest in order to balance development with greening and preservation."
+  },
+  {
+    "title" : "Brasilia",
+    "text" : "Brasilia, Brazil includes the largest national urban park in the world, protecting tropical savanna and forest in order to balance development with greening and preservation."
+  },
+  {
+    "title" : "Milano",
+    "text" : "The forestation of agricultural areas around Milan, Italy serves as an example of conservation management, now prevalent in numerous locales throughout Europe."
+  },
+  {
+    "title" : "Duisburg",
+    "text" : "Germany has increased its forest cover by more than 3%, thanks to explicit forestation policies surrounding urban areas."
+  },
+  {
+    "title" : "Pretoria, South Afria",
+    "text" : ""
+  },
+  {
+    "title" : "Nairobi",
+    "text" : "Urbanization in the Jo'burg - Pretoria corridor has greatly increased urban land use throughout Gauteng Province, placing industrial and residential activities close together as in other industrial, growing locales around the world."
+  },
+  {
+    "title" : "Addis Ababa",
+    "text" : ""
+  },
+  {
+    "title" : "Lake Urmia (Iran)",
+    "text" : "The gradual disappearance of Lake Urmia in Iran is a token of resource shifts toay, culmination from short-term human decisions combined with long-term global trends. These markers of change are near-time exemplars of long-term changes in watershed, arable land and climate dynamics that shift opportunities globally."
+  },
+  {
+    "title" : "Coral Reefs",
+    "text" : "Today we can map every coral reef, and we can visualize bleaching events annually over decades, helping to understand visually the global impact of changes in ocean chemistry now underway."
+  },
+  {
+    "title" : "Shenzhen",
+    "text" : "Pearl River Delta (Shenzhen), China"
+  },
+  {
+    "title" : "",
+    "text" : ""
+  },
+  {
+    "title" : "Sumatra fires",
+    "text" : "Fires in Sumatra, Indonesia remove forest for agriculture."
+  },
+  {
+    "title" : "Sinabung volcano",
+    "text" : "Eruption of Sinabung volcano in Sumatra, Indonesia."
+  },
+  {
+    "title" : "Beijing",
+    "text" : "Relatively clear day for Beijing, China."
+  },
+  {
+    "title" : "Beijing",
+    "text" : "Hazy day in Beijing, China."
+  },
+  {
+    "title" : "Beijing",
+    "text" : "Very hazy day in Beijing, China."
+  },
+  {
+    "title" : "Melting Snow",
+    "text" : "Melting snow, Inner Mongolia, China."
+  },
+  {
+    "title" : "Ice Floes",
+    "text" : "Ice floes off the coast of Antarctica."
+  },
+  {
+    "title" : "Iceberg calving",
+    "text" : "Large iceberg calving off the coast of Antarctica."
+  },
+  {
+    "title" : "Typhoon In-Fa",
+    "text" : "Typhoon In-Fa was the 24th category 4+ typhoon for the 2015 season, shattering the previous record of 18 such typhoons in a season.  Luckily, it did not make landfall."
   }
+
 ];
