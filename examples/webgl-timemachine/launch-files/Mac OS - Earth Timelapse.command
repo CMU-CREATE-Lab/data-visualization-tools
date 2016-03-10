c98ada7fa48e69b4d2c7b472de1df167dc9a36a2
Paul Dille <pdille@cmucreatelab.org>
2016-03-10 17:06:26 +0000

    - Updated launch scripts:   * Handle different launch modes (maximized, kiosk, etc)   * Include virtual keyboard Chrome extension in startup

--- examples/webgl-timemachine/launch-files/Mac OS - Earth Timelapse.command
+++ examples/webgl-timemachine/launch-files/Mac OS - Earth Timelapse.command
@@ -8,8 +8,19 @@ then
 fi
 BROWSER_SCALE_FACTOR=`sed -n 's/"browserScaleFactor" : //p' $ROOT_PATH/config.js | sed "s/,.*//"`
 BROWSER_SCALE_FACTOR=$((BROWSER_SCALE_FACTOR+0))
+
+LAUNCH_MODE=`sed -n 's/"launchMode" : //p' $ROOT_PATH/config.js | sed "s/,.*//"`
+if [ $LAUNCH_MODE == "kiosk" ]
+then
+  EXTENSION_ARGS=--load-extension=%cd%app/libs/virtual-keyboard/1.11.1_0
+  LAUNCH_MODE=--kiosk
+else
+  EXTENSION_ARGS=
+  LAUNCH_MODE=--start-maximized
+fi
+
 LAUNCH_PATH="file:///$ROOT_PATH/app/data-visualization-tools/examples/webgl-timemachine/index.html"
-ARGS="--video-threads=$NUMBER_OF_PROCESSORS --allow-file-access-from-files --start-maximized --device-scale-factor=$BROWSER_SCALE_FACTOR --user-data-dir=/tmp --no-first-run"
+ARGS="--video-threads=$NUMBER_OF_PROCESSORS --allow-file-access-from-files $LAUNCH_MODE --device-scale-factor=$BROWSER_SCALE_FACTOR --user-data-dir=/tmp --no-first-run $EXTENSION_ARGS"
 
 if [ -d "/Applications/Google Chrome.app/" ]
 then
--- examples/webgl-timemachine/launch-files/Windows - Earth Timelapse.bat
+++ examples/webgl-timemachine/launch-files/Windows - Earth Timelapse.bat
@@ -6,8 +6,21 @@ SETLOCAL EnableDelayedExpansion
 for /f "tokens=2 delims=:, " %%a in (' find "browserScaleFactor" ^< "config.js" ') do (
   set BROWSER_SCALE_FACTOR=%%~a
 )
+
+for /f "tokens=2 delims=:, " %%a in (' find "launchMode" ^< "config.js" ') do (
+  set LAUNCH_MODE=%%~a
+)
+
+if %LAUNCH_MODE% == kiosk (
+  set EXTENSION_ARGS=--load-extension=%cd%app/libs/virtual-keyboard/1.11.1_0
+  set LAUNCH_MODE=--kiosk
+) else (
+  set EXTENSION_ARGS=
+  set LAUNCH_MODE=--start-maximized
+)
+
 set LAUNCH_PATH=%cd%app/data-visualization-tools/examples/webgl-timemachine/index.html
-set ARGS="--video-threads=%NUMBER_OF_PROCESSORS% --allow-file-access-from-files --start-maximized --device-scale-factor=%BROWSER_SCALE_FACTOR% --user-data-dir=%tmp% --no-first-run"
+set ARGS="--video-threads=%NUMBER_OF_PROCESSORS% --allow-file-access-from-files %LAUNCH_MODE% --device-scale-factor=%BROWSER_SCALE_FACTOR% --user-data-dir=%tmp% --no-first-run %EXTENSION_ARGS%"
 
 echo Launching user installed Chrome.
 start "Chrome" chrome --app="%LAUNCH_PATH%" "%ARGS%" || goto :USER_DOES_NOT_HAVE_CHROME
