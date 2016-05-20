9502c07a3930f4ac21183ca5b726693416827653
Paul Dille <pdille@cmucreatelab.org>
2016-05-20 03:01:41 -0400

    - Added ability to pull from a local config file instead of the master config used on the hyperwall drives - Added rootTilePath option to specify where tiles are coming from - Added ajax_includes file to be used for all dataset timestamps - Removed unnecessary files

---
+++ examples/webgl-timemachine/example-configs/config-local.js
@@ -0,0 +1,19 @@
+var EARTH_TIMELAPSE_CONFIG = {
+  "version" : 5,
+  "browserScaleFactor" : 1,
+  "isHyperwall" : false,
+  "useGoogleMaps" : false,
+  "useGoogleSearch" : false,
+  "defaultPlaybackSpeed" : 0.5,
+  "enableAutoMode" : false,
+  "screenTimeoutInMilliseconds" : 4 * 60 * 1000,
+  "waypointDelayInMilliseconds" : 1 * 15 * 1000,
+  "showEVA" : false,
+  "showForestAlerts" : false,
+  "showLODES" : false,
+  "enableLetterboxMode" : false,
+  "useFaderShader" : true,
+  "enableWaypointText" : true,
+  "enableRecordingMode" : false,
+  "rootTilePath" : "http://tiles.cmucreatelab.org/earthtimelapse/data/"
+};
