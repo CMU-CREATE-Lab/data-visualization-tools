e451c302713357a7e67d5397ad4d71875ebc3646
Gabriel ODonnell <gabriel.odonnell@gmail.com>
2017-01-09 11:52:45 -0500

    [examples/webgl-timemachine] Change Annual refugees tile to use layer   * Was a single class (MonthlyRefugeesTile.js)

--- examples/webgl-timemachine/WebGLVectorTile2.js
+++ examples/webgl-timemachine/WebGLVectorTile2.js
@@ -172,6 +172,50 @@ WebGLVectorTile2.prototype._setLodesData = function(arrayBuffer) {
   }
 }
 
+WebGLVectorTile2.prototype._setMonthlyRefugeesData = function(arrayBuffer) {
+  var gl = this.gl;
+  this._pointCount = arrayBuffer.length / 10; 
+
+  if (this._pointCount > 0) {
+    this._data = arrayBuffer;
+    this._arrayBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, this._arrayBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, this._data, gl.STATIC_DRAW);
+
+    var attributeLoc = gl.getAttribLocation(this.program, 'aStartPoint');
+    gl.enableVertexAttribArray(attributeLoc);
+    gl.vertexAttribPointer(attributeLoc, 2, gl.FLOAT, false, 40, 0);
+
+    var attributeLoc = gl.getAttribLocation(this.program, 'aEndPoint');
+    gl.enableVertexAttribArray(attributeLoc);
+    gl.vertexAttribPointer(attributeLoc, 2, gl.FLOAT, false, 40, 8);
+
+    var attributeLoc = gl.getAttribLocation(this.program, 'aMidPoint');
+    gl.enableVertexAttribArray(attributeLoc);
+    gl.vertexAttribPointer(attributeLoc, 2, gl.FLOAT, false, 40, 16);
+
+    var attributeLoc = gl.getAttribLocation(this.program, 'aEpoch');
+    gl.enableVertexAttribArray(attributeLoc);
+    gl.vertexAttribPointer(attributeLoc, 1, gl.FLOAT, false, 40, 24);
+
+
+    var attributeLoc = gl.getAttribLocation(this.program, 'aEndTime');
+    gl.enableVertexAttribArray(attributeLoc);
+    gl.vertexAttribPointer(attributeLoc, 1, gl.FLOAT, false, 40, 28);
+
+
+    var attributeLoc = gl.getAttribLocation(this.program, 'aSpan');
+    gl.enableVertexAttribArray(attributeLoc);
+    gl.vertexAttribPointer(attributeLoc, 1, gl.FLOAT, false, 40, 32);
+
+    var attributeLoc = gl.getAttribLocation(this.program, 'aTimeOffset');
+    gl.enableVertexAttribArray(attributeLoc);
+    gl.vertexAttribPointer(attributeLoc, 1, gl.FLOAT, false, 40, 36);
+
+    this._ready = true;
+  }
+}
+
 WebGLVectorTile2.prototype._setAnnualRefugeesData = function(arrayBuffer) {
   var gl = this.gl;
   this._pointCount = arrayBuffer.length / 7;
@@ -521,6 +565,74 @@ WebGLVectorTile2.prototype._drawLodes = function(transform, options) {
     gl.drawArrays(gl.POINTS, 0, Math.floor(this._pointCount*throttle));
     gl.disable(gl.BLEND);
   }
+}
+
+WebGLVectorTile2.prototype._drawMonthlyRefugees = function(transform, options) {
+  if (this._ready) {
+
+    gl.useProgram(this.program);
+    gl.enable( gl.BLEND );
+    gl.blendFunc( gl.DST_ALPHA, gl.ONE_MINUS_SRC_ALPHA );
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, this._arrayBuffer);
+
+    var zoom = options.zoom || (2.0 * window.devicePixelRatio);
+    var pointSize = options.pointSize || (1.0 * window.devicePixelRatio);;
+    pointSize *= 4.0 * Math.pow(20 / 4, (options.zoom - 3) / (10 - 3));
+
+    if (isNaN(pointSize)) {
+      pointSize = 1.0;
+    }
+
+    var sizeLoc = gl.getUniformLocation(this.program, 'uSize');
+    gl.uniform1f(sizeLoc, pointSize);
+      
+    var timeLoc = gl.getUniformLocation(this.program, 'uTotalTime');
+    gl.uniform1f(timeLoc, 1296000);
+
+    var tileTransform = new Float32Array(transform);
+    scaleMatrix(tileTransform, Math.pow(2,this._tileidx.l)/256., Math.pow(2,this._tileidx.l)/256.);
+    scaleMatrix(tileTransform, this._bounds.max.x - this._bounds.min.x, this._bounds.max.y - this._bounds.min.y);
+    var matrixLoc = gl.getUniformLocation(this.program, 'uMapMatrix');
+    gl.uniformMatrix4fv(matrixLoc, false, tileTransform);
+
+    var currentTime = options.currentDate;
+    var epochLoc = gl.getUniformLocation(this.program, 'uEpoch');
+    gl.uniform1f(epochLoc, currentTime/1000.);
+
+    var attributeLoc = gl.getAttribLocation(this.program, 'aStartPoint');
+    gl.enableVertexAttribArray(attributeLoc);
+    gl.vertexAttribPointer(attributeLoc, 2, gl.FLOAT, false, 40, 0);
+
+    var attributeLoc = gl.getAttribLocation(this.program, 'aEndPoint');
+    gl.enableVertexAttribArray(attributeLoc);
+    gl.vertexAttribPointer(attributeLoc, 2, gl.FLOAT, false, 40, 8);
+
+    var attributeLoc = gl.getAttribLocation(this.program, 'aMidPoint');
+    gl.enableVertexAttribArray(attributeLoc);
+    gl.vertexAttribPointer(attributeLoc, 2, gl.FLOAT, false, 40, 16);
+
+    var attributeLoc = gl.getAttribLocation(this.program, 'aEpoch');
+    gl.enableVertexAttribArray(attributeLoc);
+    gl.vertexAttribPointer(attributeLoc, 1, gl.FLOAT, false, 40, 24);
+
+    var attributeLoc = gl.getAttribLocation(this.program, 'aEndTime');
+    gl.enableVertexAttribArray(attributeLoc);
+    gl.vertexAttribPointer(attributeLoc, 1, gl.FLOAT, false, 40, 28);
+
+    var attributeLoc = gl.getAttribLocation(this.program, 'aSpan');
+    gl.enableVertexAttribArray(attributeLoc);
+    gl.vertexAttribPointer(attributeLoc, 1, gl.FLOAT, false, 40, 32);
+
+    var attributeLoc = gl.getAttribLocation(this.program, 'aTimeOffset');
+    gl.enableVertexAttribArray(attributeLoc);
+    gl.vertexAttribPointer(attributeLoc, 1, gl.FLOAT, false, 40, 36);
+
+    gl.drawArrays(gl.POINTS, 0, this._pointCount);
+
+    gl.disable(gl.BLEND);
+  }
+
 }
 
 WebGLVectorTile2.prototype._drawAnnualRefugees = function(transform, options) {
@@ -1165,5 +1277,87 @@ WebGLVectorTile2.urbanFragilityFragmentShader =
 '  gl_FragColor = vec4(color.r, color.g, color.b, .75) * alpha;\n' +
 '}\n';
 
+WebGLVectorTile2.monthlyRefugeesVertexShader =
+    "attribute vec4 aStartPoint;\n" + 
+    "attribute vec4 aEndPoint;\n" +  
+    "attribute vec4 aMidPoint;\n" + 
+    "attribute float aEpoch;\n" + 
+    "attribute float aEndTime;\n" + 
+    "attribute float aSpan;\n" + 
+    "attribute float aTimeOffset;\n" + 
+
+    "uniform float uSize;\n" + 
+    "uniform float uEpoch;\n" + 
+    "uniform mat4 uMapMatrix;\n" + 
+    "uniform float uTotalTime;\n" + 
+
+    "float Epsilon = 1e-10;\n" +  
+    "varying vec4 vColor;\n" +  
+      
+    "vec4 bezierCurve(float t, vec4 P0, vec4 P1, vec4 P2) {\n" +
+        "return (1.0-t)*(1.0-t)*P0 + 2.0*(1.0-t)*t*P1 + t*t*P2;\n" + 
+    "}\n" + 
+
+    "vec3 HUEtoRGB(float H){\n" +
+        "float R = abs((H * 6.) - 3.) - 1.;\n" + 
+        "float G = 2. - abs((H * 6.) - 2.);\n" + 
+        "float B = 2. - abs((H * 6.) - 4.);\n" + 
+        "return clamp(vec3(R,G,B), 0.0, 1.0);\n" + 
+    "}\n" + 
+
+    "vec3 HSLtoRGB(vec3 HSL){\n" +
+        "vec3 RGB = HUEtoRGB(HSL.x);\n" + 
+        "float C = (1. - abs(2. * HSL.z - 1.)) * HSL.y;\n" + 
+        "return (RGB - 0.5) * C + HSL.z;\n" + 
+    "}\n" + 
+ 
+    "vec3 RGBtoHCV(vec3 RGB){\n" +
+        "vec4 P = (RGB.g < RGB.b) ? vec4(RGB.bg, -1.0, 2.0/3.0) : vec4(RGB.gb, 0.0, -1.0/3.0);\n" + 
+        "vec4 Q = (RGB.r < P.x) ? vec4(P.xyw, RGB.r) : vec4(RGB.r, P.yzx);\n" + 
+        "float C = Q.x - min(Q.w, Q.y);\n" + 
+        "float H = abs((Q.w - Q.y) / (6. * C + Epsilon) + Q.z);\n" + 
+        "return vec3(H, C, Q.x);\n" + 
+     "}\n" + 
+    
+     "vec3 RGBtoHSL(vec3 RGB){\n" +
+        "vec3 HCV = RGBtoHCV(RGB);\n" + 
+        "float L = HCV.z - HCV.y * 0.5;\n" + 
+        "float S = HCV.y / (1. - abs(L * 2. - 1.) + Epsilon);\n" + 
+        "return vec3(HCV.x, S, L);\n" + 
+     "}\n" + 
+
+    "vec4 calcColor(float p, vec3 c){\n" + 
+        "vec3 hsl = RGBtoHSL(c);\n" +  
+        "return vec4(HSLtoRGB(vec3(hsl.x, hsl.y, p)), 1.);\n" +  
+    "}\n" + 
+
+    "void main() {\n" +
+        "vec4 position;\n" + 
+        "if (uEpoch < aEpoch || (uEpoch > aEpoch + aSpan)) {\n" +
+            "position = vec4(-1,-1,-1,-1);\n" + 
+        "}else {\n" +
+            "float t = (uEpoch - aEpoch)/aSpan + aTimeOffset;\n" + 
+            "t = min(t, 1.);\n" + 
+            "t = max(t,0.);\n" + 
+            "vec4 pos = bezierCurve(t, aStartPoint, aMidPoint, aEndPoint);\n" + 
+            "position = uMapMatrix * vec4(pos.x, pos.y, 0, 1);\n" + 
+            "float luminance = clamp(1. - ((aEndTime - uEpoch)/uTotalTime), 0.45, 0.95);\n" + 
+            "vColor = calcColor(luminance, vec3(1.,0.,0.));\n" + 
+        "}\n" + 
+        
+        "gl_Position = position;\n" + 
+        "gl_PointSize = uSize;\n" + 
+    "}";
+
+WebGLVectorTile2.monthlyRefugeesFragmentShader =
+    "precision mediump float;\n" + 
+    "varying vec4 vColor;\n" + 
+      
+    "void main() {\n" +
+        "float dist = length(gl_PointCoord.xy - vec2(.5, .5));\n" + 
+        "dist = 1. - (dist * 2.);\n" + 
+        "dist = max(0., dist);\n" + 
+        "gl_FragColor = vColor * dist;\n" + 
+    "}";
 
 
