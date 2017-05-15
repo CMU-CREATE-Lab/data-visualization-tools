320f678337a1bd6dccf016db1ad81fc01489cc67
Paul Dille <pdille@cmucreatelab.org>
2017-05-15 00:55:08 -0400

    - Add ability to use a frozen Chrome build when launching   * Windows only

--- examples/webgl-timemachine/launch-files/Windows - Earth Timelapse.bat
+++ examples/webgl-timemachine/launch-files/Windows - Earth Timelapse.bat
@@ -5,6 +5,7 @@ SETLOCAL EnableDelayedExpansion
 
 set BROWSER_SCALE_FACTOR=1
 set LAUNCH_MODE=""
+set USE_FROZEN_CHROME=0
 
 for /f "tokens=2 delims=:, " %%a in (' find "browserScaleFactor" ^< "config.js" ') do (
   set BROWSER_SCALE_FACTOR=%%~a
@@ -12,6 +13,10 @@ for /f "tokens=2 delims=:, " %%a in (' find "browserScaleFactor" ^< "config.js" 
 
 for /f "tokens=2 delims=:, " %%a in (' find "launchMode" ^< "config.js" ') do (
   set LAUNCH_MODE=%%~a
+)
+
+for /f "tokens=2 delims=:, " %%a in (' find "useFrozenChrome" ^< "config.js" ') do (
+  set USE_FROZEN_CHROME=%%~a
 )
 
 for /f "tokens=2 delims=:, " %%a in (' find "clearProfile" ^< "config.js" ') do (
@@ -36,11 +41,17 @@ if %LAUNCH_MODE% == kiosk (
   set LAUNCH_MODE=--start-maximized
 )
 
+if %USE_FROZEN_CHROME% == true (
+  set CHROME_LAUNCH_PATH=%cd%\app\libs\GoogleChromePortable57\GoogleChromePortable.exe
+) else (
+  set CHROME_LAUNCH_PATH=chrome
+)
+
 set LAUNCH_PATH=%cd%/app/data-visualization-tools/examples/webgl-timemachine/index.html
 set ARGS="--video-threads=%NUMBER_OF_PROCESSORS% --allow-file-access-from-files %LAUNCH_MODE% --device-scale-factor=%BROWSER_SCALE_FACTOR% --user-data-dir=%tmp%/et --no-first-run %EXTENSION_ARGS%"
 
 echo Launching user installed Chrome.
-start "Chrome" chrome --app="%LAUNCH_PATH%" "%ARGS%" || goto :USER_DOES_NOT_HAVE_CHROME
+start "Chrome" %CHROME_LAUNCH_PATH% --app="%LAUNCH_PATH%" "%ARGS%" || goto :USER_DOES_NOT_HAVE_CHROME
 goto :DONE
 
 :USER_DOES_NOT_HAVE_CHROME
