3b90ae84ad9c8baea83a257a4c943317296cd48e
jcpyun <johnchpyun@gmail.com>
2017-06-05 12:32:16 -0400

    different category animation working

--- examples/webgl-timemachine/newjcp.js
+++ examples/webgl-timemachine/newjcp.js
@@ -17,31 +17,31 @@ var storydiv="";
 storydiv+='<div class="row">';
 
  // I am gonna try to implement a function for this //
-var refugeediv=""
-refugeediv+='<div class="colz-6">'
-refugeediv+='<div class="vidContainer" id="refugee_vid_button" style="z-index:0;width:100%;height:100%;" align:"center">'
-refugeediv+=    '<div class="videotext blender">Refugee Crisis</div>'
-refugeediv+=    '<video id="refugeevideo" poster="/static/img/earth.png" style="height:100%;position:relative;z-index:1;width:100%;" loop >'
-refugeediv+=        '<source src="http://img.timeinc.net/time/video/time-lapse/earth-time-lapse.ogg" type="video/ogg"/>'
-refugeediv+=    '</video>'
-refugeediv+='</div>'
-refugeediv+='</div>'
+var refugee_video_div=""
+refugee_video_div+='<div class="colz-6">'
+refugee_video_div+='<div class="vidContainer" id="refugee_vid_button" style="z-index:0;width:100%;height:100%;" align:"center">'
+refugee_video_div+=    '<div class="videotext blender">Refugee Crisis</div>'
+refugee_video_div+=    '<video id="refugeevideo" poster="/static/img/earth.png" style="height:100%;position:relative;z-index:1;width:100%;" loop >'
+refugee_video_div+=        '<source src="http://img.timeinc.net/time/video/time-lapse/earth-time-lapse.ogg" type="video/ogg"/>'
+refugee_video_div+=    '</video>'
+refugee_video_div+='</div>'
+refugee_video_div+='</div>'
 
-var pandemicsdiv=""
-pandemicsdiv+='<div class="colz-6">'
-pandemicsdiv+='<div class="vidContainer" id="pandemics_vid_button" style="z-index:0;width:100%;height:100%;" align:"center" onclick="tester()">'
-pandemicsdiv+=  '<div class="videotext blender">Pandemics</div>'
-pandemicsdiv+=  '<video id="pandemicsvideo" poster="/static/img/earth.png" style="height:100%;position:relative;z-index:1;width:100%;" loop >'
-pandemicsdiv+=    '<source src="http://img.timeinc.net/time/video/time-lapse/earth-time-lapse.ogg" type="video/ogg"/>'
-pandemicsdiv+=  '</video>'
-pandemicsdiv+='</div>'
-pandemicsdiv+='</div>'
+var pandemics_video_div=""
+pandemics_video_div+='<div class="colz-6">'
+pandemics_video_div+='<div class="vidContainer" id="pandemics_vid_button" style="z-index:0;width:100%;height:100%;" align:"center" onclick="tester()">'
+pandemics_video_div+=  '<div class="videotext blender">Pandemics</div>'
+pandemics_video_div+=  '<video id="pandemicsvideo" poster="/static/img/earth.png" style="height:100%;position:relative;z-index:1;width:100%;" loop >'
+pandemics_video_div+=    '<source src="http://img.timeinc.net/time/video/time-lapse/earth-time-lapse.ogg" type="video/ogg"/>'
+pandemics_video_div+=  '</video>'
+pandemics_video_div+='</div>'
+pandemics_video_div+='</div>'
 
 
 
-storydiv+='<div class="colz-12">Lets Go Pens</div>'
-storydiv+=refugeediv;
-storydiv+=pandemicsdiv;
+storydiv+='<div class="colz-12">CATEGORIES</div>'
+storydiv+=refugee_video_div;
+storydiv+=pandemics_video_div;
 
 
 
@@ -63,20 +63,21 @@ introdiv1+=                 '<div class="colz-4">'
 introdiv1+=storydiv;
 introdiv1+=                     '</div>'
 
-introdiv1+=                 '<div class="colz-4">'
-introdiv1+=             'EARTH </br> Timelapse </br>'
-introdiv1+=             '<button class="gbutton" id="explorebutton" style="margin-top:50px;" onclick="exploreclicked()"> Explore &nbsp &#10095</button></a>'
+introdiv1+=                 '<div class="colz-4" id="secondinitial">'
+
 introdiv1+=                 '</div>'
 introdiv1+=             '</div>'
 introdiv1+=         '</div>'
 introdiv1+=     '</div>'
+// introdiv1+='<div class="explainborderhead">wow</div>'
+introdiv1+= '<button class="gbutton" onclick="goback()" style="margin-top:-100px"> test </button>'
 introdiv1+='</div>'
 
 $("#timeMachine").append(introdiv1);
 
 
 $( "#explorebutton" ).click(); //for debugging, skips the explore button click part
-
+$(".toggleLayerPanelBtn").click(); //another debugging. closes down button
 
 
 // All of my touchstart events. This is faster than onClick by 300ms
@@ -84,15 +85,23 @@ $( "#explorebutton" ).click(); //for debugging, skips the explore button click p
 
 
 $("#refugee_vid_button").bind('touchstart click', function(){
-    alert("wow")
+    alert("refugee clicked")
+    storyclicked('refugee')
 })
 $("#pandemics_vid_button").bind('touchstart click', function(){
-    alert("wow")
+    alert("pandemics clicked")
+    storyclicked('pandemics')
 })
 
 // refugee_vid_button.bind('touchstart click', function(){
 //     alert("wow")
 // })
+
+
+var refugee_story_div=""
+refugee_story_div+='<div class="colz-12">'
+refugee_story_div+='TEST WOW'
+refugee_story_div+='</div>'
 
 
 
@@ -131,4 +140,42 @@ refugeevid.playbackRate=1;
   pandemicsvid.play();
 }
 
+function storyclicked(category){
+    if (category == 'refugee'){
+        $( "#initial" ).animate({
+                    opacity: 1,
+                    left: "-=100%",
+                    // height: "toggle"
+        }, 1000,function(){
+            console.log("complete");
+            // $('#initial').remove();
+            // $("#explaindiv").append(testdiv);
+        });
+        $("#secondinitial").html("REFUGEE WOWOWOW")
 
+    }
+    if (category == 'pandemics'){
+        $( "#initial" ).animate({
+                    opacity: 1,
+                    left: "-=100%",
+                    // height: "toggle"
+        }, 1000,function(){
+            console.log("complete");
+            // $('#initial').remove();
+            // $("#explaindiv").append(testdiv);
+        });
+        $("#secondinitial").html("WOW PENDEMICS")
+
+    }
+}
+function goback(){
+    alert("GOING BACK!!!");
+    $( "#initial" ).animate({
+                    opacity: 1,
+                    left: "+=100%",
+                    // height: "toggle"
+        }, 1000,function(){
+            console.log("complete");
+            
+        });
+}
