e86fc949d0d7bc2e431d74861bfb616940f7a85d
Gabriel ODonnell <gabriel.odonnell@gmail.com>
2017-09-15 10:04:42 -0400

    [examples/webgl-timemachine] Swap _setEbolaData with _setBufferData

--- examples/webgl-timemachine/WebGLVectorTile2.js
+++ examples/webgl-timemachine/WebGLVectorTile2.js
@@ -554,40 +554,6 @@ WebGLVectorTile2.prototype._setIomIdpData = function(data) {
   }
 }
 
-// Ebola: a_Centroid[2] a_Epoch1 a_Deaths1 a_Epoch2 a_Deaths2
-WebGLVectorTile2.prototype._setEbolaData = function(arrayBuffer) {
-  var gl = this.gl;
-  this._pointCount = arrayBuffer.length / 6;
-  if (this._pointCount > 0) {
-    this._data = arrayBuffer;
-    this._arrayBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, this._arrayBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, this._data, gl.STATIC_DRAW);
-
-    var attributeLoc = gl.getAttribLocation(this.program, 'a_Centroid');
-    gl.enableVertexAttribArray(attributeLoc);
-    gl.vertexAttribPointer(attributeLoc, 2, gl.FLOAT, false, 24, 0);
-
-    var attributeLoc = gl.getAttribLocation(this.program, 'a_Epoch1');
-    gl.enableVertexAttribArray(attributeLoc);
-    gl.vertexAttribPointer(attributeLoc, 1, gl.FLOAT, false, 24, 8);
-
-    var attributeLoc = gl.getAttribLocation(this.program, 'a_Deaths1');
-    gl.enableVertexAttribArray(attributeLoc);
-    gl.vertexAttribPointer(attributeLoc, 1, gl.FLOAT, false, 24, 12);
-
-    var attributeLoc = gl.getAttribLocation(this.program, 'a_Epoch2');
-    gl.enableVertexAttribArray(attributeLoc);
-    gl.vertexAttribPointer(attributeLoc, 1, gl.FLOAT, false, 24, 16);
-
-    var attributeLoc = gl.getAttribLocation(this.program, 'a_Deaths2');
-    gl.enableVertexAttribArray(attributeLoc);
-    gl.vertexAttribPointer(attributeLoc, 1, gl.FLOAT, false, 24, 20);
-
-    this._ready = true;
-  }
-}
-
 // Coral Reef outlines worldCoord[2]
 WebGLVectorTile2.prototype._setCoralReefData = function(arrayBuffer) {
   var gl = this.gl;
