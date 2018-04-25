5212d4b2b10ad726507ab5990827da9f4d2df00f
Paul Dille <pdille@cmucreatelab.org>
2018-04-25 02:44:03 -0400

    - Add ability to show different featured layers depending upon the chosen theme

--- js/utils.js
+++ js/utils.js
@@ -547,3 +547,13 @@ function PackArray(arr) {
     return out
 }
 
+function capitalize_each_word_in_string(str) {
+  str = str.split(" ");
+
+  for (var i = 0, x = str.length; i < x; i++) {
+      str[i] = str[i][0].toUpperCase() + str[i].substr(1);
+  }
+
+  return str.join(" ");
+}
+
