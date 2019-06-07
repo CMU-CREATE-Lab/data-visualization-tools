6aa6f1bd4046f99c794458fc74d5755decdfba3e
Paul Dille <pdille@cmucreatelab.org>
2019-06-06 22:17:04 -0400

    - Fixes to latest search changes

--- examples/webgl-timemachine/ContentSearch.js
+++ examples/webgl-timemachine/ContentSearch.js
@@ -14,6 +14,7 @@ ContentSearch.prototype.clearSearch = function() {
   this.$searchInput.val('');
   this.$layerSearchResults.hide();
   this.$clearButton.hide();
+  resizeLayersMenu();
 }
 
 ContentSearch.prototype.updateSearch = function() {
