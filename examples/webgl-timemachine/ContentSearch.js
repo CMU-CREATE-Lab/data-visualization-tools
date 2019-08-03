4833263038710fe5b5dc7681de8a977dd02f74e0
Paul Dille <pdille@cmucreatelab.org>
2019-08-03 17:35:46 -0400

    - Various UI fixes   * Ensure "No layers found" search message is hidden when search box is cleared   * Ensure layer description tooltip appears above all UI elements (fixes #83)   * Fix layer description tooltip arrow

--- examples/webgl-timemachine/ContentSearch.js
+++ examples/webgl-timemachine/ContentSearch.js
@@ -14,6 +14,7 @@ ContentSearch.prototype.clearSearch = function() {
   this.$searchInput.val('');
   this.$layerSearchResults.hide();
   this.$clearButton.hide();
+  this.$layerSearchResultsEmptyMsg.hide();
   resizeLayersMenu();
 }
 
