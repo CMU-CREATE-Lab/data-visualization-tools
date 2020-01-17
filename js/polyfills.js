6435c3d572422358300991f37a4c4c0fa8d6ffbf
Paul Dille <pdille@cmucreatelab.org>
2020-01-16 21:33:49 -0500

    - Add polyfills for Array.find and String.includes

--- js/polyfills.js
+++ js/polyfills.js
@@ -22,17 +22,21 @@
       return Date.now() - nowOffset;
     }
   }
-
-  if (!String.prototype.startsWith) {
-      String.prototype.startsWith = function(searchString, position){
-        position = position || 0;
-        return this.substr(position, searchString.length) === searchString;
-    };
-  }
 })();
 
-// For IE Math.log2 support
-Math.log2 = Math.log2 || function(x){return Math.log(x)*Math.LOG2E;};
+// For IE <= 11 Math.log2 support
+if (!Math.log2) {
+  Math.log2 = function(x) {
+    return Math.log(x) * Math.LOG2E;
+  };
+}
+
+if (!String.prototype.startsWith) {
+  String.prototype.startsWith = function(searchString, position) {
+    position = position || 0;
+    return this.substr(position, searchString.length) === searchString;
+  };
+}
 
 if (!String.prototype.endsWith) {
   String.prototype.endsWith = function(search, this_len) {
@@ -41,4 +45,63 @@ if (!String.prototype.endsWith) {
     }
     return this.substring(this_len - search.length, this_len) === search;
   };
+}
+
+if (!String.prototype.includes) {
+  String.prototype.includes = function(search, start) {
+    'use strict';
+
+    if (search instanceof RegExp) {
+      throw TypeError('first argument must not be a RegExp');
+    }
+    if (start === undefined) { start = 0; }
+    return this.indexOf(search, start) !== -1;
+  };
+}
+
+// https://tc39.github.io/ecma262/#sec-array.prototype.find
+if (!Array.prototype.find) {
+  Object.defineProperty(Array.prototype, 'find', {
+    value: function(predicate) {
+      // 1. Let O be ? ToObject(this value).
+      if (this == null) {
+        throw TypeError('"this" is null or not defined');
+      }
+
+      var o = Object(this);
+
+      // 2. Let len be ? ToLength(? Get(O, "length")).
+      var len = o.length >>> 0;
+
+      // 3. If IsCallable(predicate) is false, throw a TypeError exception.
+      if (typeof predicate !== 'function') {
+        throw TypeError('predicate must be a function');
+      }
+
+      // 4. If thisArg was supplied, let T be thisArg; else let T be undefined.
+      var thisArg = arguments[1];
+
+      // 5. Let k be 0.
+      var k = 0;
+
+      // 6. Repeat, while k < len
+      while (k < len) {
+        // a. Let Pk be ! ToString(k).
+        // b. Let kValue be ? Get(O, Pk).
+        // c. Let testResult be ToBoolean(? Call(predicate, T, << kValue, k, O >>)).
+        // d. If testResult is true, return kValue.
+        var kValue = o[k];
+        if (predicate.call(thisArg, kValue, k, o)) {
+          return kValue;
+        }
+        // e. Increase k by 1.
+        k++;
+      }
+
+      // 7. Return undefined.
+      return undefined;
+    },
+    configurable: true,
+    writable: true
+  });
 }
