dbb70f137a9be8133f00ae471ca4576ffc24f306
Gabriel ODonnell <gabriel.odonnell@gmail.com>
2020-06-09 15:54:24 -0400

    Update animated linestrings to hopefully draw on thumbnail server

--- examples/webgl-timemachine/WebGLVectorTile2.js
+++ examples/webgl-timemachine/WebGLVectorTile2.js
@@ -3946,6 +3946,8 @@ WebGLVectorTile2.prototype._drawLineStringEpoch = function(transform, options) {
       gl.drawArrays(gl.LINES, 0, this.buffers[key]["pointCount"]);
       perf_draw_points(this._pointCount);
       gl.disable(gl.BLEND);
+    } else {
+      timelapse.lastFrameCompletelyDrawn = false;
     }
   }
 }
