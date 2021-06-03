CMU-CREATE-Lab/data-visualization-tools

commit 923c8a0ca82934c747b2ec99fcb24d1c2a48164c

- Don't re-draw when the current frame hasn't change   * e.g. when paused there is no need to keep redrawing the canvas

examples/webgl-timemachine/ETMBLayer.ts
@@ -60,6 +60,7 @@ export class ETMBLayer extends LayerOptions implements LayerInterface {
     }
   }
 
+  nextFrameNeedsRedraw = true;
   layerDef: any;
   layerId: string;
   style: MapboxTypes.Style;
@@ -230,7 +231,7 @@ export class ETMBLayer extends LayerOptions implements LayerInterface {
       console.log(`SOURCE ID: ${id}`)
       syncState.mapSources[id] = source;
     }
-    
+
     syncState.mapLayers = initialStyle.layers.slice(); // clone
 
     this.map.on('error', function (e) {
@@ -258,20 +259,33 @@ export class ETMBLayer extends LayerOptions implements LayerInterface {
   }
 
   // viewBounds:  xmin, xmax, ymin, ymax all in coords 0-256
-  draw(view, opt_options) {
-    console.assert(false);
-    if (!this._shown) {
-      this._show();
-    }
-    if (this._loaded) {
-      ETMBLayer.map._render(); // no args to _render means render all layers on map
-      if (!ETMBLayer.map.areTilesLoaded()) {
-        gEarthTime.timelapse.lastFrameCompletelyDrawn = false;
-      }
-    } else {
-      gEarthTime.timelapse.lastFrameCompletelyDrawn = false;
-    }
-  }
+  // draw(view, opt_options) {
+  //   console.assert(false);
+  //   if (!this._shown) {
+  //     this._show();
+  //   }
+  //   if (this._loaded) {
+  //     ETMBLayer.map._render(); // no args to _render means render all layers on map
+  //     let nextFrameNeedsRedraw: boolean;
+  //     if (!ETMBLayer.map.areTilesLoaded()) {
+  //       gEarthTime.timelapse.lastFrameCompletelyDrawn = false;
+  //       nextFrameNeedsRedraw = true;
+  //     } else {
+  //       nextFrameNeedsRedraw = false;
+  //     }
+  //     if (!(window as any).stopit) {
+  //       console.log(`ETMBLayer draw:  nextFrameNeedsRedraw is ${nextFrameNeedsRedraw}`);
+  //     }
+  //     for (let layerProxy of gEarthTime.layerDB.drawnLayersOrSublayersInDrawOrder()) {
+  //       let layer = layerProxy.layer;
+  //       if (layer instanceof ETMBLayer) {
+  //         layer.nextFrameNeedsRedraw = nextFrameNeedsRedraw;
+  //       }
+  //     }
+  //   } else {
+  //     gEarthTime.timelapse.lastFrameCompletelyDrawn = false;
+  //   }
+  // }
 
   abortLoading() {
   }
@@ -302,10 +316,45 @@ export class ETMBLayer extends LayerOptions implements LayerInterface {
       this.copyEarthtimeViewToMapbox();
       // Don't wait for 'load' event;  we need to call _render beforehand to help the loading process.
       this.map._render(); // no args to _render means render all layers on map
+      let nextFrameNeedsRedraw: boolean;
+      if (ETMBLayer.map.loaded() && ETMBLayer.map.areTilesLoaded()) {
+        nextFrameNeedsRedraw = false;
+      } else {
+        gEarthTime.timelapse.lastFrameCompletelyDrawn = false;
+        nextFrameNeedsRedraw = true;
+      }
+      for (let layerProxy of gEarthTime.layerDB.drawnLayersOrSublayersInDrawOrder()) {
+        let layer = layerProxy.layer;
+        if (layer instanceof ETMBLayer) {
+          layer.nextFrameNeedsRedraw = nextFrameNeedsRedraw;
+        }
+      }
     } else {
       //console.log(`${this.staticLogPrefix()} map not yet loaded`);
     }
   }
+
+  //   if (this._loaded) {
+  //     ETMBLayer.map._render(); // no args to _render means render all layers on map
+  //     let nextFrameNeedsRedraw: boolean;
+  //     if (!ETMBLayer.map.areTilesLoaded()) {
+  //       gEarthTime.timelapse.lastFrameCompletelyDrawn = false;
+  //       nextFrameNeedsRedraw = true;
+  //     } else {
+  //       nextFrameNeedsRedraw = false;
+  //     }
+  //     if (!(window as any).stopit) {
+  //       console.log(`ETMBLayer draw:  nextFrameNeedsRedraw is ${nextFrameNeedsRedraw}`);
+  //     }
+  //     for (let layerProxy of gEarthTime.layerDB.drawnLayersOrSublayersInDrawOrder()) {
+  //       let layer = layerProxy.layer;
+  //       if (layer instanceof ETMBLayer) {
+  //         layer.nextFrameNeedsRedraw = nextFrameNeedsRedraw;
+  //       }
+  //     }
+  //   } else {
+  //     gEarthTime.timelapse.lastFrameCompletelyDrawn = false;
+
 
   static copyEarthtimeViewToMapbox() {
     if (this.map) {
@@ -355,7 +404,7 @@ export class ETMBLayer extends LayerOptions implements LayerInterface {
 
       if (!this._tmpldiv) {
         let tmp = document.createElement("div");
-  
+
         tmp.setAttribute("class", "main-template-div");
         tmp.style.display = "none";
         tmp.style.position = "absolute";
@@ -392,7 +441,7 @@ export class ETMBLayer extends LayerOptions implements LayerInterface {
 
             layerProps[lyr][ttl] = propertiesById[p];
           }
-          
+
         });
 
         console.log(`${this.logPrefix()} mouse over`,layerProps);
@@ -419,7 +468,7 @@ export class ETMBLayer extends LayerOptions implements LayerInterface {
           this._tmpldiv.style.border = "1px solid";
         } else {
         this._tmpldiv.style.display = "none";
-        } 
+        }
       } else {
           this._tmpldiv.style.display = "none";
       }
@@ -551,7 +600,7 @@ class MapboxEarthtimeProxyLayer {
     }
     return MapboxEarthtimeProxyLayer._cache[layerProxy.id];
   }
- 
+
 }
 
 // class LayerTemplate {
@@ -569,9 +618,9 @@ class MapboxEarthtimeProxyLayer {
 //     else
 //       opts = {
 //         aliases: templateDef.options.aliases ? templateDef.options.aliases : {},
-//         exceptions: templateDef.options.exceptions ? new Set<string>(templateDef.options.exceptions) : new Set<string>() 
+//         exceptions: templateDef.options.exceptions ? new Set<string>(templateDef.options.exceptions) : new Set<string>()
 //       }
-      
+
 //     let tmpl = templateDef?.template ? templateDef.template : `
 //     <div>
 //     {{props ~root itemVar="~parent"}}
@@ -621,7 +670,7 @@ class MapboxEarthtimeProxyLayer {
 
 //     if (this._def.options.exceptions.size > 0) {
 //       data = this._except(data, this._def.options.exceptions);
-      
+
 //       if (this._def.options.aliases.keys)
 //         this._aliasInPlace(data);
 //     }
@@ -632,7 +681,7 @@ class MapboxEarthtimeProxyLayer {
 //     try {
 //       var html = this._tmpl(data);
 //     } catch (err) {
-//       html = ""; 
+//       html = "";
 //     }
 
 //     if (html) {

examples/webgl-timemachine/Layer.ts
@@ -122,6 +122,7 @@ export class LayerOptions {
     maxGmapsZoomLevel(): number | null;
     info(): string;
     handleVisibilityStateChange(): void;
+    nextFrameNeedsRedraw: boolean;
   }
 
   export abstract class Layer extends LayerOptions implements LayerInterface {
@@ -145,6 +146,12 @@ export class LayerOptions {
     isLoaded(): boolean { return true; }; // Override this in subclass if needed
     allVisibleTilesLoaded(): boolean { return true; } // Override this in subclass if needed
     legendVisible: boolean;
+    // Does next frame update require a redraw of this layer?
+    // If a tile has been received or any other state change requires a redraw, set to true
+    // Doesn't need to be set solely because of a change of viewport or playback time;  that will cause
+    // all layers to redraw independently of this value.
+    // Should typically be set by subclass Layer.draw function to false at the beginning of redraw
+    nextFrameNeedsRedraw = true;
 
     constructor(layerProxy: LayerProxy, layerOptions: LayerOptions, tileClass: typeof Tile) {
       super(layerOptions);
@@ -285,6 +292,7 @@ export class LayerOptions {
     // viewBounds:  xmin, xmax, ymin, ymax all in coords 0-256
     // TODO: Fix this for 900913 coords
   _drawHelper(view, opt_options) {
+    this.nextFrameNeedsRedraw = false;
     if (this.ready) {
       var width = this._canvasLayer.canvas.width / this._canvasLayer.resolutionScale_;
       var height = this._canvasLayer.canvas.height / this._canvasLayer.resolutionScale_;

examples/webgl-timemachine/MediaLayer.ts
@@ -20,6 +20,7 @@ export class MediaLayer extends LayerOptions implements LayerInterface {
   $extrasContentContainerTitleBar: any;
   $extrasContentContainer: any;
   $extrasContentComponent: any;
+  nextFrameNeedsRedraw = true;
 
   constructor(layerProxy: LayerProxy, glb: Glb, canvasLayer, tileUrl: string, layerOptions: LayerOptions) {
     super(layerOptions);

examples/webgl-timemachine/index.ts
@@ -142,6 +142,10 @@ class EarthTimeImpl implements EarthTime {
   canvasLayer = null;
   readyToDraw = false;
   currentlyShownTimeline: any;
+  lastPlaybackTime = -1;
+  lastView = {x : -1, y : -1, scale: -1};
+  lastClientDimensions = {width : -1, height : -1};
+  lastDrawnLayers = [];
   async setDatabaseID(databaseID: GSheet) {
     if (loadedLayersGSheet && databaseID.url() == loadedLayersGSheet.url()) return;
     loadedLayersGSheet = databaseID;
@@ -281,10 +285,12 @@ class EarthTimeImpl implements EarthTime {
      * Layers with single timestamp (begin time = end time) show a year without a timeline.
      * And enforce that animated base layers like landsat pause that at same time.
      */
+
     let visibleLayers = this.layerDB.visibleLayers;
     let baseLayerTimeline = null;
     let dataLayerTimeline = null;
     let isFullExtent = false;
+
     for (let i = 0; i < visibleLayers.length; i++) {
       if (visibleLayers[i].layer?.category == 'Base Layers') {
         baseLayerTimeline = visibleLayers[i].layer?.timeline;
@@ -294,15 +300,16 @@ class EarthTimeImpl implements EarthTime {
         isFullExtent = true;
       }
     }
-    if (dataLayerTimeline == null) {
-      if (isFullExtent)  {
+
+    if (!dataLayerTimeline) {
+      if (isFullExtent || !baseLayerTimeline)  {
         return null;
       } else {
         return baseLayerTimeline;
       }
-    } else {
-      return dataLayerTimeline;
     }
+    return dataLayerTimeline;
+
   }
 
   playbackRates() {
@@ -3104,10 +3111,57 @@ function resizeLayersMenu() {
 // Draws to canvas.
 // Called by TimeMachineCanavasLayer during animation and/or view changes
 function update() {
+  let currentTimelapseView = gEarthTime.timelapse.getView();
+  let currentViewportWidth = gEarthTime.timelapse.getViewportWidth();
+  let currentViewportHeight = gEarthTime.timelapse.getViewportHeight();
+  let currentPlaybackTime = gEarthTime.timelapse.getCurrentTime();
+  let currentDrawnLayers = gEarthTime.layerDB.drawnLayersOrSublayersInDrawOrder();
+
+  let needRedraw = false;
+  let verboseRedrawTest = false;
+
+  for (let layerProxy of gEarthTime.layerDB.visibleLayers) {
+    if (layerProxy.layer?.nextFrameNeedsRedraw) {
+      needRedraw = true;
+      if (verboseRedrawTest) console.log(`Layer ${layerProxy.id} requests redraw`);
+      break;
+    }
+  }
+
+  if (gEarthTime.lastPlaybackTime != currentPlaybackTime) {
+    needRedraw = true;
+    if (verboseRedrawTest) console.log('Playback time changed; need redraw');
+  } else if (gEarthTime.lastView.x != currentTimelapseView.x ||
+             gEarthTime.lastView.y != currentTimelapseView.y ||
+             gEarthTime.lastView.scale != currentTimelapseView.scale) {
+    needRedraw = true;
+    if (verboseRedrawTest) console.log('View changed; need redraw');
+  } else if (gEarthTime.lastClientDimensions.height != currentViewportHeight ||
+             gEarthTime.lastClientDimensions.width != currentViewportWidth) {
+    needRedraw = true;
+    if (verboseRedrawTest) console.log('Viewport changed; need redraw');
+  } else if (gEarthTime.lastDrawnLayers.length != currentDrawnLayers.length ||
+             !gEarthTime.lastDrawnLayers.map(layerProxy => layerProxy.id).every((id,i)=> (currentDrawnLayers[i] && id == currentDrawnLayers[i].id))) {
+    needRedraw = true;
+    if (verboseRedrawTest) console.log('Layers changed; need redraw');
+  }
+
+  if (!needRedraw) return;
+
+  (window as any).perf_drawframe();
+
+  gEarthTime.lastPlaybackTime = currentPlaybackTime;
+  gEarthTime.lastView = currentTimelapseView;
+  gEarthTime.lastClientDimensions = {width: currentViewportWidth, height: currentViewportHeight};
+  gEarthTime.lastDrawnLayers = currentDrawnLayers;
+
+
   gEarthTime.timelapse.lastFrameCompletelyDrawn = true;
+
   gEarthTime.startRedraw();
+
   if (!gEarthTime.readyToDraw || !gEarthTime.layerDB) {
-    gEarthTime.timelapse.lastFrameCompletelyDrawn = true;
+    gEarthTime.timelapse.lastFrameCompletelyDrawn = false;
     return;
   }
   if (disableAnimation) {
@@ -3122,11 +3176,13 @@ function update() {
   // Set this to true at the beginning of frame redraw;  any layer that decides it wasn't completely drawn will set
   // this to false upon draw below
   // If any selected layers not yet loaded, set lastFrameCompletelyDrawn to false
-  for (let layer of gEarthTime.layerDB.visibleLayers) {
-    if (!layer.isLoaded()) {
+  for (let layerProxy of gEarthTime.layerDB.visibleLayers) {
+    if (!layerProxy.isLoaded()) {
       gEarthTime.timelapse.lastFrameCompletelyDrawn = false;
     }
   }
+
+  gEarthTime.updateTimelineIfNeeded();
 
   if (gEarthTime.layerDB.mapboxLayersAreVisible()) {
     // Ask ETMBLayer to render everything as Mapbox.  (EarthTime layers are inserted into Mapbox and drawn as custom layers)
@@ -3139,8 +3195,6 @@ function update() {
       sublayer.draw();
     }
   }
-
-  gEarthTime.updateTimelineIfNeeded();
 
   gEarthTime.showVisibleLayersLegendsAndCredits();
 

examples/webgl-timemachine/EarthTime.ts
@@ -7,6 +7,10 @@ import { Glb } from './Glb'
 import { Timeline } from './Timeline';
 
 export interface EarthTime {
+  lastClientDimensions: { width: number, height: number };
+  lastView: {[key: string]: number},
+  lastDrawnLayers: any;
+  lastPlaybackTime: number;
   defaultMasterPlaybackRate: number;
   defaultPlaybackRate: number;
   dotmapsServerHost: any;

examples/webgl-timemachine/LayerDB.ts
@@ -212,6 +212,7 @@ export class LayerDB {
         if (isLoaded) {
           cache.loadedLayers.push(layerProxy);
           let layer = layerProxy.layer;
+          layer.nextFrameNeedsRedraw = true;
           if ('getSublayers' in layer) {
             for (let [j, sublayer] of layer.getSublayers().entries()) {
               loadedSublayers.push([sublayer.drawOrder, i, j, sublayer]);

examples/webgl-timemachine/LayerTemplate.ts
@@ -38,7 +38,7 @@ export class LayerTemplate
 	readonly options: {
 		alias: {[key: string]: any},
 		exclude: any,
-		include: any		
+		include: any
 	}
 
 	constructor(
@@ -83,15 +83,15 @@ export class LayerTemplate
 
 		return new LayerTemplate(
 			{
-				active: def.hasOwnProperty("active") ? def.active : true, 
-				title: def.title ? def.title : "", 
-				template: def.template.trim() != "default" ? def.template : BaseTemplate, 
+				active: def.hasOwnProperty("active") ? def.active : true,
+				title: def.title ? def.title : "",
+				template: def.template?.trim() != "default" ? def.template : BaseTemplate,
 				options: opts
 			});
 	}
 
 	static _normalizeOpt(opt)
-	{		
+	{
 		let normalize = subopt =>
 		{
 			if (Array.isArray(subopt)) {
@@ -121,7 +121,7 @@ export class MouseOverTemplate
 
 
 	_def: LayerTemplate;
-	
+
 	_tmpl: JsViews.Template;
 
 	constructor(id: string, def: LayerTemplate)
@@ -130,7 +130,7 @@ export class MouseOverTemplate
 		this.active = def.active;
 		this._def = def
 
-		if (def.template.trim())
+		if (def.template?.trim())
 		{
 			try {
 				this._tmpl = $.templates({
@@ -194,7 +194,7 @@ export class MouseOverTemplate
 			if (html) {
 				console.log(`Template HTML BEFORE: ${html}`)
 
-				if (Object.keys(this._def.options.alias).length > 0) 
+				if (Object.keys(this._def.options.alias).length > 0)
 					html = this._alias(html);
 
 				if (this._def.title.trim()) {
@@ -213,7 +213,7 @@ export class MouseOverTemplate
 			}
 
 			console.log(`Template HTML AFTER: ${html}`)
-			return this.div;	
+			return this.div;
 		}
 	}
 
@@ -272,7 +272,7 @@ export class MouseOverTemplate
 	// 		{
 	// 			if (!(label in exclude))
 	// 			{
-	// 				copy[label] = dict[label].constructor != Object ? dict[label] : innerExclude(dict[label]); 
+	// 				copy[label] = dict[label].constructor != Object ? dict[label] : innerExclude(dict[label]);
 	// 			}
 	// 		}
 
@@ -335,7 +335,7 @@ export class MouseOverTemplate
 	// 		{
 	// 			if ((label in include))
 	// 			{
-	// 				copy[label] = dict[label].constructor != Object ? dict[label] : innerInclude(dict[label]); 
+	// 				copy[label] = dict[label].constructor != Object ? dict[label] : innerInclude(dict[label]);
 	// 			}
 	// 		}
 

examples/webgl-timemachine/Timeline.ts
@@ -12,10 +12,6 @@ import { Utils } from "./Utils";
 
 export type TimelineType = "customUI" | "defaultUI";
 
-// TODO: make this work right for Timemachine layer
-//           be able to pass in all timestamps
-//           is there a time where we have a Timemachine layer but not yet know its capture times, before tm.json loaded?
-
 export class Timeline {
     timelineType: TimelineType;
     startDate: string;

commit ed182a22afb94d00e1d9c3cc626b6a3870f49fb2

- Further tweaks around layer draw ready states   * Should hopefully finally address regression from commit 923c8a0ca8

examples/webgl-timemachine/ETMBLayer.ts
@@ -81,7 +81,7 @@ export class ETMBLayer extends LayerOptions implements LayerInterface {
 
   // NOTE: this always returns true since we can't tell whether individual
   // layers have loaded all tiles
-  allVisibleTilesLoaded(): boolean {
+  allTilesLoaded(): boolean {
     return true;
   }
 

examples/webgl-timemachine/Layer.ts
@@ -144,7 +144,7 @@ export class LayerOptions {
     legend?: Legend;
     paired: boolean;
     isLoaded(): boolean { return true; }; // Override this in subclass if needed
-    allVisibleTilesLoaded(): boolean { return true; } // Override this in subclass if needed
+    allTilesLoaded(): boolean { return true; } // Override this in subclass if needed
     legendVisible: boolean;
     // Does next frame update require a redraw of this layer?
     // If a tile has been received or any other state change requires a redraw, set to true
@@ -292,8 +292,8 @@ export class LayerOptions {
     // viewBounds:  xmin, xmax, ymin, ymax all in coords 0-256
     // TODO: Fix this for 900913 coords
   _drawHelper(view, opt_options) {
-    this.nextFrameNeedsRedraw = false;
     if (this.ready) {
+      this.nextFrameNeedsRedraw = false;
       var width = this._canvasLayer.canvas.width / this._canvasLayer.resolutionScale_;
       var height = this._canvasLayer.canvas.height / this._canvasLayer.resolutionScale_;
       var options = opt_options ?? {};
@@ -310,6 +310,8 @@ export class LayerOptions {
       this._tileView.setView(view, width, height, this._canvasLayer.resolutionScale_);
       if (this.drawLayerFunction) options = this.drawLayerFunction(options);
       this._tileView.update(transform, options);
+    } else {
+      this.nextFrameNeedsRedraw = true;
     }
   }
 

examples/webgl-timemachine/MediaLayer.ts
@@ -67,7 +67,7 @@ export class MediaLayer extends LayerOptions implements LayerInterface {
     }
   }
 
-  allVisibleTilesLoaded(): boolean {
+  allTilesLoaded(): boolean {
     return true;
   }
 
@@ -126,6 +126,8 @@ export class MediaLayer extends LayerOptions implements LayerInterface {
       var image = document.getElementById("extras-image") as HTMLImageElement;
       image.addEventListener('load', function() {
         that.ready = true;
+        gEarthTime.timelapse.lastFrameCompletelyDrawn = true;
+        that.nextFrameNeedsRedraw = false;
       });
       image.src = filePath;
     } else if (fileType == "video") {
@@ -152,6 +154,8 @@ export class MediaLayer extends LayerOptions implements LayerInterface {
       }
       video.addEventListener('loadeddata', function() {
         that.ready = true;
+        gEarthTime.timelapse.lastFrameCompletelyDrawn = true;
+        that.nextFrameNeedsRedraw = false;
       });
       video.src = filePath;
       // Must set playbackRate *after* setting the file path
@@ -168,6 +172,8 @@ export class MediaLayer extends LayerOptions implements LayerInterface {
       var iframe = document.getElementById("extras-iframe") as HTMLIFrameElement;
       iframe.addEventListener('load', function() {
         that.ready = true;
+        gEarthTime.timelapse.lastFrameCompletelyDrawn = true;
+        that.nextFrameNeedsRedraw = false;
       });
       iframe.src = filePath;
     }

examples/webgl-timemachine/WebGLVectorLayer2.ts
@@ -45,7 +45,7 @@ export class WebGLVectorLayer2 extends Layer {
     this._drawHelper(view, opt_options);
   }
 
-  allVisibleTilesLoaded(): boolean {
+  allTilesLoaded(): boolean {
     var tiles = this.getTiles();
     if (!tiles || Object.keys(tiles).length == 0) {
       return false;

examples/webgl-timemachine/index.ts
@@ -336,7 +336,7 @@ class EarthTimeImpl implements EarthTime {
       if (loadedLayersInIdOrder[i].layer.mapType == "mapbox" && !foundMapbox) {
         foundMapbox = true;
       }
-      if (loadedLayersInIdOrder[i].layer.hasLegend && !loadedLayersInIdOrder[i].layer.legendVisible && loadedLayersInIdOrder[i].layer.allVisibleTilesLoaded()) {
+      if (loadedLayersInIdOrder[i].layer.hasLegend && !loadedLayersInIdOrder[i].layer.legendVisible && loadedLayersInIdOrder[i].layer.allTilesLoaded()) {
         this.layerDB.layerFactory.setLegend(loadedLayersInIdOrder[i].id);
       }
     }
@@ -3146,6 +3146,9 @@ function update() {
              !gEarthTime.lastDrawnLayers.map(layerProxy => layerProxy.id).every((id,i)=> (currentDrawnLayers[i] && id == currentDrawnLayers[i].id))) {
     needRedraw = true;
     if (verboseRedrawTest) console.log('Layers changed; need redraw');
+  } else if (!gEarthTime.timelapse.lastFrameCompletelyDrawn) {
+    if (verboseRedrawTest) console.log('lastFrameCompletelyDrawn was false; need redraw');
+    needRedraw = true;
   }
 
   gEarthTime.lastPlaybackTime = currentPlaybackTime;
@@ -3170,7 +3173,7 @@ function update() {
   // this to false upon draw below
   // If any selected layers not yet loaded, set lastFrameCompletelyDrawn to false
   for (let layerProxy of gEarthTime.layerDB.visibleLayers) {
-    if (!layerProxy.isLoaded() || !layerProxy.layer || !layerProxy.layer.allVisibleTilesLoaded()) {
+    if (!layerProxy.isLoaded()) {
       gEarthTime.timelapse.lastFrameCompletelyDrawn = false;
     }
   }